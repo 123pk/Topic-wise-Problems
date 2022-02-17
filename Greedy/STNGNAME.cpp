f493575f08a251a3a577c93fc0decb2149362331
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-17 20:29:50 +0530

    Create STNGNAME.cpp

---
+++ Greedy/STNGNAME.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codechef
+Contest :- Codeforces Starters 26 
+Approach :- First sort the characters of both the Alice and Bob string and now greedily choose on their turn and add desired character to the solution at desired position
+          * Alice want the resultant string to be Lexiographically smaller so he will choose the smallest characters to be positoned in the smallest index possible
+          * Bob want the resultant string to  be Lexiographically larger so he will choose the bigger character to be positioned in the largest index possible
+*/
