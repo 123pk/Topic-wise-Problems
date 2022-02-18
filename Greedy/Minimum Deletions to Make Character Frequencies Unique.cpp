f0546dbf65e70bb45b4c6613eb89d44526da742a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-18 21:15:52 +0530

    Update Minimum Deletions to Make Character Frequencies Unique.cpp

--- Greedy/Minimum Deletions to Make Character Frequencies Unique.cpp
+++ Greedy/Minimum Deletions to Make Character Frequencies Unique.cpp
@@ -6,3 +6,41 @@ Approach :-  I will find the maximum frequency of character in string , that is 
              (fre[i]-i) characters . Once I reach to 1 then I have to delete all the remaining characters and add frequency to answer.
 
 */
+class Solution {
+public:
+    int minDeletions(string s) {
+        vector<int>fre(26);
+        int mx = 0;
+        for(auto&x:s){
+            fre[x-'a']++;
+            mx = max(mx,fre[x-'a']);
+        }
+        
+        //I dont care which character is what 
+        //sort them in decreasing
+        sort(fre.begin(),fre.end(),greater<int>());
+        
+         
+        
+        //Now I will giving frequency 
+        int ans = 0;
+        for(int i = 0;i<26;++i){
+            if(fre[i]==0)break;
+            if(mx==0){
+                //only option is to delete all the characters
+                ans+=fre[i];
+            }else{
+                 if(fre[i]<=mx){
+                     mx = fre[i]-1;
+                 }else{
+                     ans+=(fre[i]-mx);
+                     mx--;
+                 }
+            }
+            //cout<<fre[i]<<" "<<ans<<" "<<mx<<"\n";
+        }
+        
+        return ans;
+        
+    }
+};
