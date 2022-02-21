61c41ba256c84560e24b9a0d8acbe471a4402836
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-21 19:03:36 +0530

    Create E - Ranges on Tree.cpp

---
+++ Tree/E - Ranges on Tree.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner Contest 240
+Approach :- We assign the values ( ith leaf has {i,i} range ) to leaf nodes then we accumulate the result for their parents . For finding leaf we use BFS and for assinging the 
+           [l,r] range we use DFS . 
+*/
