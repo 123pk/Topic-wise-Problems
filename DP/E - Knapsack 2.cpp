672748c6b8cd08cc79b6e24cdf33bdb886c4e09b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-25 22:42:00 +0530

    Create E - Knapsack 2.cpp

---
+++ DP/E - Knapsack 2.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Atcoder
+Contest :- DP task
+Approach :- For value in range from [1 to 10^5] we will find the min weight reaquired to reach upto that vaule and at the end we will find the max value which has required weight 
+            is less that max weight
+*/
