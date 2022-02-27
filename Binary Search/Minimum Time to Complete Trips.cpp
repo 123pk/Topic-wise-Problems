d529eed2914606265e198836a9de0a780761a058
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-27 20:38:28 +0530

    Create Minimum Time to Complete Trips.cpp

---
+++ Binary Search/Minimum Time to Complete Trips.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 282
+Approach :- Since the value of time is in range from 1 to 10^14 atmax , we will do binary search on the values and find the smallest value that satisfy the condition 
+*/
