9e56d321ffb39af781520477974c399afb30a7e4
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-28 20:27:38 +0530

    Update 1970. Last Day Where You Can Still Cross.cpp

--- Binary Search/1970. Last Day Where You Can Still Cross.cpp
+++ Binary Search/1970. Last Day Where You Can Still Cross.cpp
@@ -2,4 +2,6 @@
 Platform :- Leetcode
 Approach :- Binary search on values from 0 to size - 1 , We will check for each of the (start+end)/2 , if it is possible to cross then we will update the answer and binary search on
             value from (mid,end) else we binary search on value from (strat,mid).
+            
+            We will use BFS to find if it is possible to cross or not.
 */
