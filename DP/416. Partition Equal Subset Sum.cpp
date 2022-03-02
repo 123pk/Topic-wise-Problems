b11f30ae3bb471a0edc3ce3e1ba3f8664c8799e8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-02 06:50:54 +0530

    Create 416. Partition Equal Subset Sum.cpp

---
+++ DP/416. Partition Equal Subset Sum.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- Since we need to partition our array into equal sum them the total_sum of array need to be even else we can't partition them into two ,
+            now the problem is reduced to checking if there is subset of sum  = (total_sum/2), we will use knapsack dp to find this .
+*/
