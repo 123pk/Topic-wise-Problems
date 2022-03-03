572b9cf2745eec199bdb793b5b6544f5e81cb606
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-03 22:34:12 +0530

    Update 413. Arithmetic Slices.cpp

--- Two pointer/413. Arithmetic Slices.cpp
+++ Two pointer/413. Arithmetic Slices.cpp
@@ -1,4 +1,5 @@
 /*
-Platform :-
-Approach :- 
+Platform :- Leetcode
+Approach :- We try to find length of arithmetic subarray , for a arithmetic array of length of 'c' we can have (c*(c+1))/2 subarrays . So we find that 'c' while traverseing through
+            the array and maintain one value 'c' which we increment if (A[i]-A[i-1] == dif) else we add (c*(c+1))/2 to our answer and make c = 0 and (dif = A[i]-A[i-1])
 */
