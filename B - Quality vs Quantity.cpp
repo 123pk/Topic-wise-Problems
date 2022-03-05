aad593b652e2987b5f26abe6c7bc38b9b1334a97
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-05 13:48:29 +0530

    Update and rename B - Quality vs Quantity.cpp to Two pointer/B - Quality vs Quantity.cpp

--- B - Quality vs Quantity.cpp
+++
@@ -1,7 +0,0 @@
-/*
-Platform :- Codeforces
-Contest :- Codeforces Round 774 Div 2
-
-
-SOLUTION WILL BE ADDED ONCE CONTEST IS OVER
-*/
---
+++ Two pointer/B - Quality vs Quantity.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 774 Div 2
+Approach :- So we calculate the prefix sum for the given array , and we sort the array and basic idea is to color largest values with red ,let the count of those be 'c' then check
+            if we have prefix sum of values of 'c+1' smallest element is lesss than the sum of largest 'c' values then answer exist else no.
+*/
