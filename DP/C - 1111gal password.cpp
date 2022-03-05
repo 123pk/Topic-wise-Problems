9aa58f6a4f5924aedcfcd3c5c5583f582b89404f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-05 21:43:34 +0530

    Create C - 1111gal password.cpp

---
+++ DP/C - 1111gal password.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner Contest 242
+Approach :- It is standard recrusive problem, where we will use memoisation(dp) to make it more efficient. For each value from '1' to '9' we have three option either we can have
+            i-1 ,i or i+1 where i-1>=1 and i+1<=9 
+*/
