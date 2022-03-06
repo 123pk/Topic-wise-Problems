bc988186114d5216f43b9b71f3c2074128ed53ab
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-06 21:09:47 +0530

    Update 2195. Append K Integers With Minimal Sum.cpp

--- Maths/2195. Append K Integers With Minimal Sum.cpp
+++ Maths/2195. Append K Integers With Minimal Sum.cpp
@@ -7,3 +7,53 @@ Approach :- 1. Sort the array in non decreasing order ,
             4. The forumulae for finding sum of 'n' consecutive numbers is (n*(n+1))/2
             5. We use the above formulae and find the max no of values that we can get and cost of adding them.
 */
+class Solution {
+public:
+    long long minimalKSum(vector<int>& nums, int k) {
+        long long ans = 0;
+        long long last = 0;
+        sort(nums.begin(),nums.end());
+        long long val = 0,suml = 0,sumr = 0,l = 0,r = 0;
+        
+        for(int i=0;i<nums.size();++i){
+            val = (nums[i]-last)-1;
+            
+            if(val<=0){
+                last = nums[i];
+                continue;
+            }
+            
+            if(val>k){
+                //sum till min value
+                suml = (last*(last+1))/2;
+                //sum till max values that is till (last+k)
+                r =last+k;
+                sumr = (r*(r+1))/2;
+                ans+=(sumr-suml);
+                k=0;
+                break;
+            }else{
+                k-=val;
+                //sum till min value
+                suml = (last*(last+1))/2;
+                
+                r = last+val;
+                //sum till max values that is till r
+                sumr = (r*(r+1))/2;
+                ans+=(sumr-suml);
+                
+                if(k==0)break;
+            }
+            last = nums[i];
+        }
+       // cout<<k<<" "<<ans<<" "<<last<<"\n";
+        if(k){
+            suml = (last*(last+1))/2;
+            r = (last+k);
+            sumr = (r*(r+1))/2;
+            ans+=(sumr-suml);
+        }
+        
+        return ans;
+    }
+};
