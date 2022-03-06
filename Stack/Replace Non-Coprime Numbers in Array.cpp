6b6b5d5b2c676bbe3ebc5edf475d26b15b9f45a8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-06 21:06:29 +0530

    Update Replace Non-Coprime Numbers in Array.cpp

--- Stack/Replace Non-Coprime Numbers in Array.cpp
+++ Stack/Replace Non-Coprime Numbers in Array.cpp
@@ -8,3 +8,57 @@ Approach :- You need to carefully analyse the problem, you will find that a grou
             We use stack for this. We push the first element of our array in stack and now onwards we check if the top element of stack have GCD equal to 1 with nums[i] then we push
             nums[i] in stack else we update the top value with the LCM of two and similarly change other elements of stack until the top element and current LCM dont have GCD 1
 */
+class Solution {
+public:
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        vector<int>ans;
+        stack<int>hold;
+        int last;
+        long long val,temp,z;
+        for(int i=0;i<nums.size();++i){
+            if(hold.size()==0){
+                hold.push(nums[i]);
+            }
+            else{
+                int temp = hold.top();
+                if(__gcd(temp,nums[i])>1){
+                    last = nums[i];
+                    while(1){
+                        temp = hold.top();
+                        val = temp;
+                        z = last;
+                        val*=z;
+                        val/=__gcd(temp,last);
+                        last = val;
+                        hold.pop();
+                        //hold.push(val);
+                        if(hold.size()){
+                            temp = hold.top();
+                            if(__gcd(temp,last)>1)continue;
+                            else{
+                                hold.push(last);
+                                break;
+                            }
+                        }
+                        else{
+                            hold.push(last);
+                            break;
+                        }
+                        
+                    }
+                }
+                else{
+                    hold.push(nums[i]);
+                }
+            }
+        }
+        
+        while(!hold.empty()){
+            ans.push_back(hold.top());
+            hold.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        
+        return ans;
+    }
+};
