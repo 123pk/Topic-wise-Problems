78a0a8498439bb76dc26e0e7746e26395879ecd9
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-07 21:39:18 +0530

    Create Merge two sorted list.cpp

---
+++ Linked list/Merge two sorted list.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- We greedily maintain two pointer and compare between values the pointer is pointer , first pointer is for first linked list
+            Second pointer is for scond linked list and choose the one to added before which have less value
+*/
