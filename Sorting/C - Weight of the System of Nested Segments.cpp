804a320f0440844d1b724b47c0ab49a651893208
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-08 22:49:10 +0530

    Update C - Weight of the System of Nested Segments.cpp

--- Sorting/C - Weight of the System of Nested Segments.cpp
+++ Sorting/C - Weight of the System of Nested Segments.cpp
@@ -1,5 +1,5 @@
 /*
 Platform :- Codeforces
 Contest :- Codeforces Round 776 Div 3
-Approach :- 
+Approach :- Find our the smalesst (2*n) weights and correspoinding coordinates , now sort the coordinates and map 'i' coordinate with '(2*n-i)' coordinate for 1 based indexing
 */
