d287375343677995886e17f90fcfac4f728f36c7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-10 22:14:13 +0530

    Update and rename DECOMPOSITON.cpp to Brute force/DECOMPOSITON.cpp

---
+++ Brute force/DECOMPOSITON.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codechef
+Contest :- Codeforces Starters 29
+Approach :- Perform all the operations given in problem and as directed and update the value , do take care of mod 
+*/
--- DECOMPOSITON.cpp
+++
@@ -1,6 +0,0 @@
-/*
-Platform :- Codechef
-Contest :- Codeforces Starters 29
-
-SOLUTION WILL BE ADDED ONCE CONTEST IS OVER
-*/
