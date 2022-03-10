8b544e2242abef975013b9905521c79bf2cc38d4
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-10 22:12:26 +0530

    Update A - Playoff.cpp

--- Maths/A - Playoff.cpp
+++ Maths/A - Playoff.cpp
@@ -3,3 +3,18 @@ Platform :- Codeforces
 Contest :- Codeforces Educational Round 124
 Approach :- We will always have the winner that is the largest odd value from (1 to 2^n) ,  which is 2^n-1
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        int ans = (1<<n)-1;
+        cout<<ans<<"\n";
+    }
+}
