cd62dc6aa1e42aab776633985bcce83f318a6dec
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-10 22:16:23 +0530

    Update and rename String protocol.cpp to Greedy/String protocol.cpp

---
+++ Greedy/String protocol.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codechef
+Contest :- Codechef Starters
+Approach :- We can have max value of our solution be 'n' in this case we have no adjacent pairs that are equal , else we find the no of no overlapping adjacent pairs and let is
+            be 'c' the our solution is (n-'c')
+*/
--- String protocol.cpp
+++
@@ -1,7 +0,0 @@
-/*
-Platform :- Codechef
-Contest :- Codechef Starters
-
-
-SOLUTION WILL BE ADDED ONCE CONTEST IS OVER
-*/
