4744dd8257419e78b345ff4b15de13734bf60e5e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-11 22:18:37 +0530

    Update C. Madoka and Childish Pranks.cpp

--- Greedy/C. Madoka and Childish Pranks.cpp
+++ Greedy/C. Madoka and Childish Pranks.cpp
@@ -6,3 +6,51 @@ Approach :- It is always optimal to come from n,m to (1,1) and we have '1' in ou
             --> if we have no cells with '1' then we have '0' operations to perform
             --> we will perform 'e' operations where 'e' is no of '1' in our matrix , for a values which is '1' at j = 0, in this case we choose (i-1,j) to (i,j) rectangle
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,m;
+        cin>>n>>m;
+        
+        
+        char ch[n][m];
+        int e = 0;
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                cin>>ch[i][j];
+                if(ch[i][j] == '1'){
+                    e++;
+                }
+            }
+        }
+        
+        if(e == 0){
+            cout<<0<<"\n";
+        }
+        else{
+            if(ch[0][0] == '1'){
+                cout<<"-1\n";
+            }
+            else{
+                 
+                cout<<(e)<<"\n";
+               
+                for(int i = n-1;i>=0;--i){
+                    for(int j = m-1;j>=0;--j){
+                        if(ch[i][j] == '0')continue;
+                        if(j==0){
+                            cout<<(i)<<" "<<(j+1)<<" "<<(i+1)<<" "<<(j+1)<<"\n";
+                        }else{
+                            cout<<(i+1)<<" "<<(j)<<" "<<(i+1)<<" "<<(j+1)<<"\n";
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
