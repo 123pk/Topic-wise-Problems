41bfed17e4f6444b0a3b9ebfb76d5728c6262036
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-12 20:43:55 +0530

    Update C - Collision 2.cpp

--- Two pointer/C - Collision 2.cpp
+++ Two pointer/C - Collision 2.cpp
@@ -9,3 +9,72 @@ Approach :- For collision we need two things ,
               ii. second one to store the range of values (x - coordinate) of person moving right.
             Now we will check for each value of 'y' if (largest value of coordinate moveing left )> smallest value of coordinate moving right or not . and that is our answer.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    
+    long long x,y;
+    map<long long,pair<long long,long long>>left,right;
+    vector<pair<long long ,long long>>P;
+    for(int i=0;i<n;++i){
+        cin>>x>>y;
+        P.push_back({x,y});
+    }
+    
+    string s;
+    cin>>s;
+    map<long long,int >l,r;
+    for(int i=0;i<n;++i){
+        if(s[i]=='L'){
+            if(l[P[i].second]==0){
+                l[P[i].second]++;
+                left[P[i].second] = {P[i].first,P[i].first};
+            }
+            else{
+                left[P[i].second].first = min(left[P[i].second].first,P[i].first);
+                left[P[i].second].second = max(left[P[i].second].second,P[i].first);
+            }
+        }
+        else{
+            if(r[P[i].second]==0){
+                r[P[i].second]++;
+                right[P[i].second] = {P[i].first,P[i].first};
+            }
+            else{
+                right[P[i].second].first = min(right[P[i].second].first,P[i].first);
+                right[P[i].second].second = max(right[P[i].second].second,P[i].first);
+            }
+        }
+    }
+    
+    //for each value we will look if there is a value in right corrensponding to this
+    int f = 0;
+    set<long long>z;
+    
+    for(int i=0;i<n;++i){
+        if(s[i]=='L'){
+            //I want a value with left coordinate
+            if(r[P[i].second]){
+                if(right[P[i].second].first<P[i].first){
+                    f=1;break;
+                }
+            }
+        }
+        else{
+            if(l[P[i].second]){
+                if(left[P[i].second].second>P[i].first){
+                    f=1;break;
+                }
+            }
+        }
+    }
+    
+    if(f)cout<<"Yes\n";
+    else{
+        cout<<"No\n";
+    }
+         
+}
