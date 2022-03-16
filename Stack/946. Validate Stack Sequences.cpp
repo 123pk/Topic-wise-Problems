59fa86dde8970e19ddcce5c9aad1379237f545e6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-16 22:37:00 +0530

    Create 946. Validate Stack Sequences.cpp

---
+++ Stack/946. Validate Stack Sequences.cpp
@@ -0,0 +1,4 @@
+/*
+Platform :- Leetcode
+Approach :- We will traverse through array push and push the elemnts in stack and then we will check if top element is equal to pop value then we pop that element from stack
+*/
