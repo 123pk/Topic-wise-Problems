d520463148c1163aaa44a3981c2d0dda8ff510d2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-16 22:37:15 +0530

    Update 946. Validate Stack Sequences.cpp

--- Stack/946. Validate Stack Sequences.cpp
+++ Stack/946. Validate Stack Sequences.cpp
@@ -2,3 +2,18 @@
 Platform :- Leetcode
 Approach :- We will traverse through array push and push the elemnts in stack and then we will check if top element is equal to pop value then we pop that element from stack
 */
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int>P;
+        int i=0,j=0;
+        for(i=0;i<pushed.size();++i){
+            P.push(pushed[i]);
+            while(!P.empty() && P.top()==popped[j]){
+                P.pop();
+                j++;
+            }
+        }
+        return P.empty();
+    }
+};
