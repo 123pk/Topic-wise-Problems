be0c062f8e1049006c5c7f21387d4dc6f37978fb
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-17 22:21:30 +0530

    Update 856. Score of Parentheses.cpp

--- Stack/856. Score of Parentheses.cpp
+++ Stack/856. Score of Parentheses.cpp
@@ -2,3 +2,27 @@
 Platform :- Leetcode
 Approach :- We try to find value of each of valid substring of parenthesis and for each level we add them and for lower level we multiply by '2' .
 */
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int>P;
+        P.push(0);
+        int ans = 1;
+        for(auto&x:s){
+            if(x == '(')P.push(0);
+            else{
+                int val = P.top();
+                P.pop();
+                if(val == 0){
+                    val+=1;
+                }
+                else{
+                    val<<=1;
+                }
+                P.top()+=val;
+            }
+        }
+        ans = P.top();
+        return ans;
+    }
+};
