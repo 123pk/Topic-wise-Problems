08d582da251bb3cec2a21bcfc306827af506ea15
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-19 23:18:35 +0530

    Update ADJLOVE.cpp

--- Maths/ADJLOVE.cpp
+++ Maths/ADJLOVE.cpp
@@ -5,3 +5,50 @@ Approach :- if we have odd no of consecutive odd numbers then the sum of product
             it is not possible , or if we have '0' odd number or all odd number then also we have no possible permutation.
             else we take all the even values in beginning and odd at the end , if we have odd no of odd numbers then we add one odd number to beginning ,before even numbers
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+         int n;
+         cin>>n;
+         
+         int c = 0;
+         long long A[n];
+         for(int i=0;i<n;++i){
+             cin>>A[i];
+             if(A[i]%2ll)c++;
+             
+         }
+         
+         int d = n-c;
+         
+         if(c == 1)cout<<"-1\n";
+         else if((c%2 && c==n) || c==0)cout<<"-1\n";
+         else{
+             vector<long long>odd,even;
+             for(auto&x:A){
+                 if(x%2)odd.push_back(x);
+                 else even.push_back(x);
+             }
+             
+             //we choose the even indexes first and odd at the end
+             if(c%2){
+                 cout<<odd[0]<<" ";
+                 for(auto&x:even)cout<<x<<" ";
+                 for(int i=1;i<odd.size();++i)cout<<odd[i]<<" ";
+                 cout<<"\n";
+             }
+             else{
+                 for(auto&x:odd)cout<<x<<" ";
+                 for(auto&x:even)cout<<x<<" ";
+                 cout<<"\n";
+             }
+         }
+         
+         
+    }
+}
