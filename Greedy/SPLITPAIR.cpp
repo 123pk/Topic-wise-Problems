fac8ab07de4bc5a9b54e47e0bdc785b29de279e6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-19 23:08:08 +0530

    Create SPLITPAIR.cpp

---
+++ Greedy/SPLITPAIR.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codechef
+Contest :- Codechef Lunctime March 2022
+Approach :- We will look for the last digit , if the last digit is odd then we need another odd digit and same we need more than one even digit if last digit is even
+            if the above conditions is satisfied then answer is "YES" else "NO"
+*/
