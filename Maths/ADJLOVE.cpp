43178cb52da37e3938b7c3acb6d6e579aec009e7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-19 23:18:19 +0530

    Create ADJLOVE.cpp

---
+++ Maths/ADJLOVE.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codechef
+Contest :- Codechef Lunctime march 2022
+Approach :- if we have odd no of consecutive odd numbers then the sum of product is even , so we want to have even odd no cosecutively , if we have only one odd no then 
+            it is not possible , or if we have '0' odd number or all odd number then also we have no possible permutation.
+            else we take all the even values in beginning and odd at the end , if we have odd no of odd numbers then we add one odd number to beginning ,before even numbers
+*/
