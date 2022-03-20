946ba2a4ecb9288e18ab6cde56cacc0c2f6de95a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-20 20:04:53 +0530

    Create B - Prefix Removals.cpp

---
+++ Constructive/B - Prefix Removals.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 778(Div 1 + Div 2)
+Approach :- We will traverse through the array and it is optimal to choose a prefix of size '1' and remove it , so we will look for the first character where our frequency
+            of that character is '1' , all the characters from that character to the end cannot be removed and are left.
+*/
