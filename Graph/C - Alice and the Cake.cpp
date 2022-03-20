f2437ea25b79e0e6a58baa3877c6fce5730695f2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-20 20:17:02 +0530

    Update C - Alice and the Cake.cpp

--- Graph/C - Alice and the Cake.cpp
+++ Graph/C - Alice and the Cake.cpp
@@ -6,3 +6,52 @@ Approach :- We will start in reverse, from initial value we will try to get the 
             of current element of queue is '0' then we don't have that element in our array and we need to divide it , we will do the operation (n-1) time, if at the end
             we have some element whose frequency is not '0' then we cannot obtain that array and answer is "NO" else "YES".
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        long long A[n];
+        long long tot = 0;
+        map<long long,int>fre;
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            tot+=A[i];
+            fre[A[i]]++;
+            //A[i] = -A[i];
+        }
+        //we will do in reverse from max to here
+        int f = 0;
+        int lim = n;
+        queue<long long>P;
+        P.push(tot);
+        while(lim){
+            long long temp = P.front();
+            P.pop();
+            if(fre[temp]){
+                fre[temp]--;
+            }
+            else{
+                //this is unwanted and we want to split this
+                P.push(temp/2);
+                P.push((temp+1)/2);
+                lim--;
+            }
+        }
+        
+        for(auto&x:fre){
+            if(x.second>0){
+                f=1;break;
+            }
+        }
+        
+        if(f)cout<<"NO\n";
+        else cout<<"YES\n";
+    }
+}
