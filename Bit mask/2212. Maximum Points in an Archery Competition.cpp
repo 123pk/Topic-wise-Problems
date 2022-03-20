46e9ab197828854fbdf94d16ff7e450a0c8e920d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-20 20:39:55 +0530

    Create 2212. Maximum Points in an Archery Competition.cpp

---
+++ Bit mask/2212. Maximum Points in an Archery Competition.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 285
+Approach :- Since there are 12 option for us only then we will try all possilbe combination using bitmask , and the one which give us the max value and bobArrows are <=
+             Alice arrows then we choose that option.
+*/
