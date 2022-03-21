6caa8c746c08083b7aef62144163b8beecc60e13
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-21 19:46:08 +0530

    Create Tree Diameter.cpp

---
+++ Tree/Tree Diameter.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- CSES
+Approach :- Choose any arbitary point and let us assume it be root , now find the farthest point from that point and it will work as one end of diameter , now from that
+            point find the farthest point in tree and that will give us the second point of diameter and no of edges in it is our required length of diameter.
+            We will use DFS to find the farthest point.
+*/
