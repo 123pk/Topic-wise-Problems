e2f217656f9cd4dc1dedb747a78ec46ee99f73bf
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-23 15:03:48 +0530

    Update 2209. Minimum White Tiles After Covering With Carpets.cpp

--- DP/2209. Minimum White Tiles After Covering With Carpets.cpp
+++ DP/2209. Minimum White Tiles After Covering With Carpets.cpp
@@ -6,3 +6,33 @@ Approach :- We will use dp for memoisation , so basic idea is to use dp[1000][10
             We have two options ,either to put a tile at ith index or not , if we don't put it at ith index and 's[i] = '1'' then we have one uncoverd white tile.
             min of two option we return.
 */
+class Solution {
+public:
+    int dp[1000][1001];
+    
+    int find(int idx,int rem,int len,string&s){
+        if(idx<0)return 0;
+        
+        if(dp[idx][rem]!=-1){
+            return dp[idx][rem];
+        }
+        
+        int ans = INT_MAX;
+        if(rem){
+            ans = min(ans,find(idx-len,rem-1,len,s));    
+        }
+        
+        int x = s[idx]-'0';
+        ans = min(ans,find(idx-1,rem,len,s)+x);
+        
+        return dp[idx][rem] = ans;
+        
+    }
+    
+    int minimumWhiteTiles(string floor, int numCarpets, int carpetLen) {
+        memset(dp,-1,sizeof(dp));
+        int n = floor.size();
+        return find(n-1,numCarpets,carpetLen,floor);
+        
+    }
+};
