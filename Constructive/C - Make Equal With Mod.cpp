9ba88c886d4212756466609d49d1cfbdf9fbb5c8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-24 22:23:23 +0530

    Update C - Make Equal With Mod.cpp

--- Constructive/C - Make Equal With Mod.cpp
+++ Constructive/C - Make Equal With Mod.cpp
@@ -11,3 +11,66 @@ Approach :- So this problem is all about analysing it ,
             5. if we have min vaue as 1 then if we have two adjacent values in sorted array having absolute different == 1 , then also we don't have any solution
             
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+         
+        
+        long long A[n];
+        map<long long,int>P;
+        long long mn = 1e10;
+        int c = 0;
+        int o = 0,z = 0;
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            if(A[i] == A[0])c++;
+            mn = min(mn,A[i]);
+            if(A[i] == 0)z++;
+            if(A[i] == 1)o++;
+           // P[A[i]]++;
+        }
+        int f = 0;
+        if(z&&o){
+            cout<<"NO\n";
+            continue;
+        }
+     //   cout<<c<<" "<<mn<<"\n";
+        if(c == n || mn>1 ){
+            f=1;
+        }else{
+            if(mn == 1){
+               
+        
+            sort(A,A+n);
+            int g = 0;
+            
+            long long last = 1;
+            for(int i=0;i<n;++i){
+                if(A[i]==last)continue;
+                else{
+                   // cout<<A[i]<<" "<<last<<" \n";
+                    if((A[i]-last) <= 1){
+                        g=1;break;
+                    }
+                    last = A[i];
+                }
+            }
+            if(g==0){
+                f=1;
+            }
+            
+            }
+            else f=1;
+        }
+        
+        if(f)cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+}
