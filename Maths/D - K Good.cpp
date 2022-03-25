0b875bd8c55fb95df3cd098cea4ef87ad32316c5
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-25 22:04:46 +0530

    Create D - K Good.cpp

---
+++ Maths/D - K Good.cpp
@@ -0,0 +1,9 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces CodeTon (Div1+Div2)
+Explanation :- It is purely based on math and observation ,for all odd values we have always a solution for k ='2'.
+               1. For all power of 2 we dont have any solution
+               2. For all other even values we will find values.
+               
+               Link :- https://codeforces.com/blog/entry/101211
+*/
