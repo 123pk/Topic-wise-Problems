04000151585e7224a84ada34567ed9a585f22419
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-26 21:47:07 +0530

    Update D - Polynomial Division.cpp

--- Maths/D - Polynomial Division.cpp
+++ Maths/D - Polynomial Division.cpp
@@ -6,3 +6,55 @@ Approach :- We have a poynomial Sequence A and B , the product of those is C , W
             values ( Cn+m-1 , A[n] , A[n-1] and B[m]) hence we can find B[m-1] , similarly we can get for all the other values . We will start form last value because it is 
             guaranteed that those values cannot be '0' for all test cases .
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+//vector<int>dp[100001];
+ 
+
+int main(){
+     int n,k;
+     cin>>n>>k;
+     n++;
+     k++;
+     int m = n+k-1;
+      
+     vector<long long>A(n),C(m);
+     for(int i=0;i<n;++i){
+         cin>>A[i];
+     }
+     
+     for(int i=0;i<m;++i){
+         cin>>C[i];
+     }
+     reverse(C.begin(),C.end());
+     reverse(A.begin(),A.end());
+    // int k = m-n;
+     vector<long long>B(k,INT_MIN);
+     B[0] = C[0]/A[0];
+  //   else B[0] = 0;
+     for(int i=1;i<m;++i){
+         long long z = 0;
+         int idx = -1;
+         long long tot = 0;
+         for(int l = 0;l<=i;++l){
+             int dif = i-l;
+             if((l<n) && dif<k){
+                 if(B[dif] == INT_MIN){
+                     z = A[l];
+                     idx = dif;
+                 }
+                 else{
+                     tot+=(A[l]*B[dif]);
+                 }
+             }
+         }
+       //  cout<<idx<<" "<<C[i]<<" "<<tot<<" "<<z<<endl;
+         if(idx>=0)  B[idx] = (C[i]-tot)/z;
+             
+         //}
+     }
+     reverse(B.begin(),B.end());
+     for(auto&x:B)cout<<x<<" ";
+     cout<<"\n";
+}
