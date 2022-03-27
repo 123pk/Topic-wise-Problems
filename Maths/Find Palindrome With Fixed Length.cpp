f74657669a21866f789c334eae9560cb7031ba58
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-27 22:09:08 +0530

    Create Find Palindrome With Fixed Length.cpp

---
+++ Maths/Find Palindrome With Fixed Length.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 286
+Approach :- You need to analyse it carefully and you will find that , you can have always have first half of palindrome and that is (10^(n-1) - 1) + query[i] ,
+            now you will get the second half from this .
+*/
