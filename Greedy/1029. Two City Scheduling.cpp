184eabf5a1d0246ed66bee83533d5eed48547483
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-28 21:58:53 +0530

    Create 1029. Two City Scheduling.cpp

---
+++ Greedy/1029. Two City Scheduling.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Approach :- We will first send every passanger to city 1, we will also compute the difference in cost of travelling to city 1 than city 2 , then we will sort the arrray
+            and smallest 'n' values will be added to our cost that will depict that thos passangers are sent to city 2 . It is optimal as in this way we will thos passangers
+            for city 2 whose fair for city 1 > city2 .
+*/
