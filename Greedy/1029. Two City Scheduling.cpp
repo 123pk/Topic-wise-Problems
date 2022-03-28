4cd723a4bd9c3819fe7524b3c87cab16f06ac5a8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-28 21:59:12 +0530

    Update 1029. Two City Scheduling.cpp

--- Greedy/1029. Two City Scheduling.cpp
+++ Greedy/1029. Two City Scheduling.cpp
@@ -4,3 +4,24 @@ Approach :- We will first send every passanger to city 1, we will also compute t
             and smallest 'n' values will be added to our cost that will depict that thos passangers are sent to city 2 . It is optimal as in this way we will thos passangers
             for city 2 whose fair for city 1 > city2 .
 */
+class Solution {
+public:
+     
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        int ans = 0;
+        int tot = 0;
+        vector<int>dif;
+        for(auto&x:costs){
+            dif.push_back(x[1]-x[0]);
+            tot+=x[0];
+        }
+        sort(dif.begin(),dif.end());
+        int n = costs.size();
+        n>>=1;
+        for(int i = 0;i<n;++i){
+            tot+=dif[i];
+        }
+        ans = tot;
+        return ans;
+    }
+};
