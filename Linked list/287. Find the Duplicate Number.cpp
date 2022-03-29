c5cb8465fe28f96e1dedaf83b0e2a4fa271837d7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-29 23:08:11 +0530

    Create 287. Find the Duplicate Number.cpp

---
+++ Linked list/287. Find the Duplicate Number.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- the idea is same as ditecting cycle in a linked list where we use two pointer one is slow and one is fast which hops two places to next whenever they became equal
+            we return that element.
+*/
