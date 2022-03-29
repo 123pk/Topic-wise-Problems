60e1451699c85ad7742afbec928b623990521eb2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-03-29 23:08:54 +0530

    Update 287. Find the Duplicate Number.cpp

--- Linked list/287. Find the Duplicate Number.cpp
+++ Linked list/287. Find the Duplicate Number.cpp
@@ -3,3 +3,28 @@ Platform :- Leetcode
 Approach :- the idea is same as ditecting cycle in a linked list where we use two pointer one is slow and one is fast which hops two places to next whenever they became equal
             we return that element.
 */
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums)
+{
+	if (nums.size() > 1)
+	{
+		int slow = nums[0];
+		int fast = nums[nums[0]];
+		while (slow != fast)
+		{
+			slow = nums[slow];
+			fast = nums[nums[fast]];
+		}
+
+		fast = 0;
+		while (fast != slow)
+		{
+			fast = nums[fast];
+			slow = nums[slow];
+		}
+		return slow;
+	}
+	return -1;
+}
+};
