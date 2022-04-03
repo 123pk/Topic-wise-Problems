d5bb49e82f432b52cf6355c7d81073b436eb1019
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-03 22:02:58 +0530

    Update F1. Promising String (easy version).cpp

--- Greedy/F1. Promising String (easy version).cpp
+++ Greedy/F1. Promising String (easy version).cpp
@@ -1,4 +1,7 @@
 /*
 Platform :- Codeforces
 Contest :- Codeforces Round 780 Div 3
+Approach :- We will maintain a prefix array which will store no of increments we can do till ith index ,( change '--' to '+') .
+            Now we will traverse through all the subarrays possible of length from 2 to n and will check if the no of ('+') == no of('-') then we will increment our answer
+            as they are already good , else we will check if we have enough option to change '--' to '+' such that they are equal.
 */
