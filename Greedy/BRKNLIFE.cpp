14c1464b2fdbe2960c19d3621044aaaf4a01e8e4
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-06 23:22:30 +0530

    Create BRKNLIFE.cpp

---
+++ Greedy/BRKNLIFE.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codechef
+Contest :- Codechef Starters 33
+Approach :- If 'A' is subsequenece of 'S' then there is no way we can get 'S1' in this case we print -1
+            else we find the missing character starting from left to right while checking for subsequence , and similarly form right to left 
+            we ignore these two characters and choose any characters different from this and fill them in '?'
+*/
