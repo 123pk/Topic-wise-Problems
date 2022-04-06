f6d581f8bc853793f02cbfbd1c96a00e0eef7c6b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-06 23:23:59 +0530

    Update BRKNLIFE.cpp

--- Greedy/BRKNLIFE.cpp
+++ Greedy/BRKNLIFE.cpp
@@ -5,3 +5,61 @@ Approach :- If 'A' is subsequenece of 'S' then there is no way we can get 'S1' i
             else we find the missing character starting from left to right while checking for subsequence , and similarly form right to left 
             we ignore these two characters and choose any characters different from this and fill them in '?'
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        
+        int n,m;
+        cin>>n>>m;
+        
+         string s,p;
+         cin>>s>>p;
+         
+         
+         //if our string is already a subsequence then we cannot trasform
+         //check if p is subsequence or not
+         char ch = '@';
+         int j = 0;
+         for(int i=0;i<n && j<m;++i){
+             if(s[i] == p[j]){
+                 j++;
+             }
+         }
+         
+         if(j == m){
+             cout<<"-1\n";
+         }
+         else{
+             ch = p[j];
+             
+             //now reverse check
+             char ch2='@';
+             j = m-1;
+             for(int i=n-1;i>=0 && j>=0;--i){
+                 if(s[i] == p[j]){
+                     j--;
+                 }
+             }
+             ch2 = p[j];
+            // vector<char>P;
+             char start = 'a';
+             for(;start<='e';start++){
+                 if(ch!=start && start!=ch2){
+                     ch = start;
+                     break;
+                 }
+             }
+             for(int i=0;i<n;++i){
+                 if(s[i] == '?')cout<<ch;
+                 else cout<<s[i];
+             }
+             cout<<"\n";
+         }
+         
+    }
+}
