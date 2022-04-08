a4326572a117ca4bf35437108b3c1f9190f8ff39
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-08 17:59:00 +0530

    Update Removing  Digits.cpp

--- DP/Removing  Digits.cpp
+++ DP/Removing  Digits.cpp
@@ -1,4 +1,5 @@
 /*
 Platform :- CSES
 Approach :- So we will start with bottom up dp , where dp[i] stores the minimum no of operations require to get ith value from '0' by adding digits 
+            dp[i] = min(dp[i-j] + 1,dp[i])  where 'j' is the digit in 'i'
 */
