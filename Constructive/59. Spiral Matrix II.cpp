6124094de60e46a5627eca05e40f9025b749f459
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-13 21:22:52 +0530

    Create 59. Spiral Matrix II.cpp

---
+++ Constructive/59. Spiral Matrix II.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- We will use a pointer which will help us in changing direction , few things to take care of the values of matrix cannot be more than (n*n) where n = no of rows
+            second thing while changing direction we will take care of boundaries like (i<n && j<n) for 0 based indexing.
+*/
