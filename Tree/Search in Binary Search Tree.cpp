0abb7a19328a67aa76e83babb43298be3fcc8251
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-14 22:00:25 +0530

    Create Search in Binary Search Tree.cpp

---
+++ Tree/Search in Binary Search Tree.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- Since it is a BST we know that all the values to the left are smaller than current value of node and all the values to the right are greater 
+            we will look for value if we found the values we will get the values in subtree and retun it
+*/
