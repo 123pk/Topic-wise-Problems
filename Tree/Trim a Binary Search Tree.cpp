c7eed69ed9c20e9aae8cef9862f29072ff8552d5
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-15 22:44:14 +0530

    Update Trim a Binary Search Tree.cpp

--- Tree/Trim a Binary Search Tree.cpp
+++ Tree/Trim a Binary Search Tree.cpp
@@ -3,3 +3,29 @@ Platform :- Leetcode
 Approach :- This is problem similar to deleting a node from BST , we will first find all the values to be removed and then it becomes same we need to remove all of them
             from BST, few things to node after each deleting the parents need to be updated.
 */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        while(root != NULL && (root->val < low || root->val > high)){
+            if(root->val < low)root = root->right;
+            else root = root->left;
+        }
+        if(root == NULL)return NULL;
+        TreeNode* left = trimBST(root->left, low, high);
+        TreeNode* right = trimBST(root->right, low, high);
+        root->left = left;
+        root->right = right;
+        return root;
+    }
+};
