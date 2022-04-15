71a50903cf7ace830c3763a8f561988f8b6e6eea
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-15 22:43:11 +0530

    Create Trim a Binary Search Tree.cpp

---
+++ Tree/Trim a Binary Search Tree.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- This is problem similar to deleting a node from BST , we will first find all the values to be removed and then it becomes same we need to remove all of them
+            from BST, few things to node after each deleting the parents need to be updated.
+*/
