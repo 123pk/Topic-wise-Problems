acc5ea0129064563e5af9b18acde30e1722fef2e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-16 22:35:24 +0530

    Update BFOC.cpp

--- Greedy/BFOC.cpp
+++ Greedy/BFOC.cpp
@@ -5,3 +5,29 @@ Approach :- We will start distributing candies from the last frined (nth one) , 
             friends from (i) to (candies+i) ans they will not be able to get even 1 candi, so we will perform the same operation till the time no of candies are >=(k)
             and we have not reached the starting child.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n;
+        long long candies,box;
+        cin>>n>>candies>>box;
+        
+        long long rem = candies;
+        long long start = n+box-1;
+        start = min(start,candies);
+        while(rem>=box && start>=box){
+            if(rem>=start){rem = rem%start;
+                start--;
+            }
+            else {
+                start = min(start,rem);
+            }
+        }
+        cout<<rem<<"\n";
+    }
+}
