f1cf9c7e75d58087a4b83ea26ac7e3ad201d75b5
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-16 22:35:12 +0530

    Update BFOC.cpp

--- Greedy/BFOC.cpp
+++ Greedy/BFOC.cpp
@@ -1,4 +1,7 @@
 /*
 Platform :- Codchef
 Contest :- Codechef Lunctime April 2022
+Approach :- We will start distributing candies from the last frined (nth one) , if for ith friend the no of buckets is > candies available then we will ignore all the
+            friends from (i) to (candies+i) ans they will not be able to get even 1 candi, so we will perform the same operation till the time no of candies are >=(k)
+            and we have not reached the starting child.
 */
