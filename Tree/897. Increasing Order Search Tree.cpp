9d11b7759ae521dd692c364ef6f255af2ba93b0a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-17 22:24:35 +0530

    Create 897. Increasing Order Search Tree.cpp

---
+++ Tree/897. Increasing Order Search Tree.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- We will first get all the values of tree and sort them in increasing order , second option is to traverse in "inorder" and get the values they will already be
+            in sorted order, now we create a new tree with all the reuqirements , basically we make all the right child to null and attach with left child only.
+*/
