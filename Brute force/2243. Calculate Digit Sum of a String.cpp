63b85579490ca6f7dd51d05d0d72753bf3bfb3e5
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-17 22:29:21 +0530

    Update 2243. Calculate Digit Sum of a String.cpp

--- Brute force/2243. Calculate Digit Sum of a String.cpp
+++ Brute force/2243. Calculate Digit Sum of a String.cpp
@@ -3,3 +3,22 @@ Platform :- Leetcode
 Contest :- Leetcode Weekly contest 289
 Approach :- We will perform the operations as stated until we get a string with length smaller than 'k'
 */
+class Solution {
+public:
+    string digitSum(string s, int k) {
+        string ans="",temp="";
+        while(s.size()>k){
+            temp = "";
+            //first took the k characters and find the sum then add and merge
+            for(int i=0;i<s.size();i+=k){
+                int val = 0;
+                for(int j=0;j<k && (i+j)<s.size();++j){
+                    val+=s[i+j]-'0';
+                }
+                temp+=to_string(val);
+            }
+            s = temp;
+        }
+        return s;
+    }
+};
