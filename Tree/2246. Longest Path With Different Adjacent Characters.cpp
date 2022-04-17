6baea29202be7be1c0e32c51c53446bc63329668
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-17 22:38:28 +0530

    Create 2246. Longest Path With Different Adjacent Characters.cpp

---
+++ Tree/2246. Longest Path With Different Adjacent Characters.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 289
+Approach :- We will do dfs and while returning or backtracking we will also get information regarding maximum no of different characters we can get if we choose that child
+           of curren node , we will store the max of all in current node of all values received and we have also option to have a path between two sibling including current
+            node.
+*/
