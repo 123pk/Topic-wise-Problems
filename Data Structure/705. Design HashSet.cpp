10e552d7e99c4b320c484eb970046c403fa4f2a0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-21 22:21:30 +0530

    Update 705. Design HashSet.cpp

--- Data Structure/705. Design HashSet.cpp
+++ Data Structure/705. Design HashSet.cpp
@@ -1,1 +1,42 @@
 
+class MyHashSet {
+public:
+    /** Initialize your data structure here. */
+   set<int>P;
+    MyHashSet() {
+      
+    }
+    
+    void add(int key) {
+        if(P.find(key)==P.end())
+        {
+            P.insert(key);
+        }
+        
+    }
+    
+    void remove(int key) {
+        if(P.find(key)!=P.end())
+        {  auto it=P.find(key);
+         P.erase(it);  
+        }
+        
+    }
+    
+    /** Returns true if this set contains the specified element */
+    bool contains(int key) {
+       if(P.find(key)==P.end())
+        {
+            return false;
+        }
+        return true;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
