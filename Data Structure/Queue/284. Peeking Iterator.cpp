7ceff0581c759717a3f64afbf93675891d8cd415
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-25 22:42:10 +0530

    Update 284. Peeking Iterator.cpp

--- Data Structure/Queue/284. Peeking Iterator.cpp
+++ Data Structure/Queue/284. Peeking Iterator.cpp
@@ -5,3 +5,30 @@ Approach :- We will use queue data structure to implement the desired iterator
             --> for returning next value we will first get the element and earase it from queue and return the delted element
             --> if queue is not empty we will return true else false in ( is Next())
 */
+class PeekingIterator : public Iterator {
+public:
+    queue<int>P;
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    for(auto&x:nums)P.push(x);
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        return P.front();
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+        int val = P.front();
+        P.pop();
+	    return val;
+	}
+	
+	bool hasNext() const {
+	    return !P.empty();
+	}
+};
