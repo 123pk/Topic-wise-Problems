e917e898bab021e457f678084030bdefafe579da
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-25 22:40:47 +0530

    Create 284. Peeking Iterator.cpp

---
+++ Data Structure/Queue/284. Peeking Iterator.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Leetcode
+Approach :- We will use queue data structure to implement the desired iterator
+            --> Queue front() to peek
+            --> for returning next value we will first get the element and earase it from queue and return the delted element
+            --> if queue is not empty we will return true else false in ( is Next())
+*/
