ae59e90b0704f4e096bd31907ff464fdd8cebed0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-26 22:03:48 +0530

    Create Controlled Inflation.cpp

---
+++ DP/Controlled Inflation.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Coding Competition with google
+Contest :- Google Codejam 2022 Round 1B
+Approach :- We will try to bruteforce all the permutations possibls for array of each people and then using dp we will try to find optimal soluton ,
+*/
