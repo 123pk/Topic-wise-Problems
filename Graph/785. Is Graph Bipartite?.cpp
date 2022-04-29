70ba70dbc1a283ad06d64aad0f7cc065956887ae
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-29 21:33:37 +0530

    Update 785. Is Graph Bipartite?.cpp

--- Graph/785. Is Graph Bipartite?.cpp
+++ Graph/785. Is Graph Bipartite?.cpp
@@ -10,3 +10,50 @@ Approach :- So since we want to divide our graph into two disjoint segment such 
             
 Time Complexity :- O(n), as we are visiting any node atmax once 
 */
+class Solution {
+public:
+    
+    bool find (int x,vector<vector<int>>&graph,vector<int>&vis){
+        //we will visit or color all the nodes that are not colored starting from node 'x'
+        queue<pair<int,int>>bfs;
+        bfs.push({x,1});
+        vis[x] = 1;
+        
+        while(!bfs.empty()){
+            int d = bfs.size();
+            for(int i=0;i<d;++i){
+                pair<int,int> temp = bfs.front();
+                bfs.pop();
+                
+                int node = temp.first;
+                int color = temp.second;
+                int option;
+                if(color == 1)option = 2;
+                else option = 1;
+                for(auto&x:graph[node]){
+                    //if two adjacent nodes are of same color then graph is not bipartite
+                    if(vis[x]){
+                        if(vis[x]!=option)return false;
+                    }
+                    else {
+                        vis[x] = option;
+                        bfs.push({x,option});
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int>vis(n);
+        
+        
+        for(int i=0;i<n;++i){
+            if(vis[i])continue;
+            if(!find(i,graph,vis))return false;
+        }
+        return true;
+    }
+};
