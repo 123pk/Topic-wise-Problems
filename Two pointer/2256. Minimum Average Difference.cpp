27958cf7fc519d2bd0e07ec1bca81a9329ad3218
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-30 21:59:16 +0530

    Create 2256. Minimum Average Difference.cpp

---
+++ Two pointer/2256. Minimum Average Difference.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Bi Weekly contest 77
+Approach :- We will find the total sum of array ,now we will traverse through the array and find the left average and right average , so the index with minimum value is 
+            our answer.
+*/
