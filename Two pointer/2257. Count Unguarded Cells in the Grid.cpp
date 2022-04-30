50c6d5157d240619917d7270cebd22e675ba7663
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-04-30 21:56:21 +0530

    Update 2257. Count Unguarded Cells in the Grid.cpp

--- Two pointer/2257. Count Unguarded Cells in the Grid.cpp
+++ Two pointer/2257. Count Unguarded Cells in the Grid.cpp
@@ -9,3 +9,98 @@ Approach :- We will perform four traversal operations and that will cover all th
             
             Four traversals are ( Left --> Right ) ,(Right --> left) , ( Top --> Bottom) and (Bottom to Top) 
 */
+class Solution {
+public:
+    int countUnguarded(int n, int m, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        int ans = 0;
+        vector<vector<int>>vis(n,vector<int>(m));
+      //  vector<vector<int>>grid(n,vector<int>(m));
+        for(auto&x:guards){
+            vis[x[0]][x[1]] = 1;
+        }
+        
+        for(auto&x:walls){
+            vis[x[0]][x[1]] = 2;
+        }
+        
+        //left to right
+        for(int i=0;i<n;++i){
+            int f = 0;
+            for(int j=0;j<m;++j){
+                if(vis[i][j] == 2){
+                    f=0;
+                }
+                else {
+                    if(vis[i][j] == 1)f=1;
+                    else{
+                        if(f)vis[i][j] = 3;
+                    }
+                    
+                }
+            }
+        }
+        
+        //now move rigt to left
+        for(int i=0;i<n;++i){
+            int f = 0;
+            for(int j=m-1;j>=0;--j){
+                if(vis[i][j] == 2){
+                    f=0;
+                }
+                else {
+                    if(vis[i][j] == 1)f=1;
+                    else{
+                        if(f)vis[i][j] = 3;
+                    }
+                    
+                }
+            }
+        }
+        
+        //lets go top down
+        for(int j=0;j<m;++j){
+            int f = 0;
+            for(int i=0;i<n;++i){
+                if(vis[i][j] == 2){
+                    f=0;
+                }
+                else {
+                    if(vis[i][j] == 1)f=1;
+                    else{
+                        if(f)vis[i][j] = 3;
+                    }
+                    
+                }
+            }
+        }
+        //bottom  up
+        for(int j=0;j<m;++j){
+            int f = 0;
+            for(int i=n-1;i>=0;--i){
+                if(vis[i][j] == 2){
+                    f=0;
+                }
+                else {
+                    if(vis[i][j] == 1)f=1;
+                    else{
+                        if(f)vis[i][j] = 3;
+                    }
+                    
+                }
+            }
+        }
+        
+        //count the cells that are not guarded 
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(vis[i][j] == 0){
+                    ans++;
+                    
+                }//cout<<vis[i][j]<<" ";
+            }
+            //cout<<"\n";
+        }
+        
+        return ans;
+    }
+};
