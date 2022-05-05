2575005d4ed48cd8c8e41c5a0383ae3cf91843fc
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-05 22:20:48 +0530

    Create A. Food for Animals.cpp

---
+++ Maths/A. Food for Animals.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 787 
+Approach :- We will try to find extra requirement to be fullfilled for dog and cat and if that value is greater than universal value then we cannot feed then else we can
+*/
