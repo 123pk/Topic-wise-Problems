8d7f5fceef92a2bba014bdcb394dec9b9036054e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-05 10:01:15 +0530

    Update and rename XYZ.cpp to Dictionary.cpp

---
+++ Brute force/Dictionary.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 786 Div 3
+Approach :- We will get all the valid strings possible using two for loops from ('a' to 'z' in both) we will ignore when we have same character in both loop 
+            else we will increment index variable which will initially start from zero ,as soon as we find our required string we will come out of loops and 
+            print the index .
+*/
--- Brute force/XYZ.cpp
+++
@@ -1,4 +0,0 @@
-/*
-Platform :- Codeforces
-Contest :- Codeforces Round 786 Div 3
-*/
