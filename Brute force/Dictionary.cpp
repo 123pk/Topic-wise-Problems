968fab81a2d4f0c2afa953455b41b59756b6dc90
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-05 10:01:25 +0530

    Update Dictionary.cpp

--- Brute force/Dictionary.cpp
+++ Brute force/Dictionary.cpp
@@ -5,3 +5,33 @@ Approach :- We will get all the valid strings possible using two for loops from 
             else we will increment index variable which will initially start from zero ,as soon as we find our required string we will come out of loops and 
             print the index .
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        string s;
+        cin>>s;
+        
+        int ans = 0;
+        int f = 0;
+        for(char ch1='a';ch1<='z';++ch1){
+            
+            for(char ch2='a';ch2<='z';++ch2){
+                string temp ="";
+                temp+=ch1;
+                temp+=ch2;
+                if(ch1 == ch2)continue;
+                ans++;
+                if(temp == s){
+                    f=1;break;
+                }
+            }
+            if(f)break;
+        }
+        cout<<ans<<"\n";
+    }
+}
