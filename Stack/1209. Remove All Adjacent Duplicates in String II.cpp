dc9c39a3aa674b07e3114cff21a6b7df0dc5133d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-06 21:26:40 +0530

    Create 1209. Remove All Adjacent Duplicates in String II.cpp

---
+++ Stack/1209. Remove All Adjacent Duplicates in String II.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- We will use stack in this , we will push the characters in stack with the frequency , if we have same element as that of top character of stack then we increment
+            the frequency of top character, as soon as the frequency becomes 'k'we pop out that element .At the end we get the string according to frequecy and return.
+*/
