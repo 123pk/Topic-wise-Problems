1b335bc3cfc48ee6369af303e18ff39ee0f0c0f0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-08 22:56:32 +0530

    Create A - Tokitsukaze and All Zero Sequence.cpp

---
+++ Greedy/A - Tokitsukaze and All Zero Sequence.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 789 Div 2
+Approach :- If there is any '0' in array then the answer is no of non zero values (let it be 'x' ),
+            if we have only unque element then ansewr is x+1 else 'x'
+*/
