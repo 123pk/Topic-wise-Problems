ceb2d35ae99cacc5865fe921513ea95a25a19459
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-09 21:46:49 +0530

    Create B1 - Tokitsukaze and Good 01-String (easy version).cpp

---
+++ Two pointer/B1 - Tokitsukaze and Good 01-String (easy version).cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 789 Div2 
+Approach :- make an array of integers which store the size of contiguous substring size of same characters ,now traverse from left to right and the difference between odd 
+            size index is added to answer , we dont use the used size.
+*/
