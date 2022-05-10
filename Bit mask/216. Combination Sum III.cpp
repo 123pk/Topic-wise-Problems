45e8f827010655b24af967e1a3367d75fb42e07a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-10 21:52:35 +0530

    Create 216. Combination Sum III.cpp

---
+++ Bit mask/216. Combination Sum III.cpp
@@ -0,0 +1,4 @@
+/*
+Platform :- Leetcode
+Approach :- First we will try all the possible combination of choosing 'k' values from '1' to '9' and if the sum is equal to 'n' we will push it in our answer.
+*/
