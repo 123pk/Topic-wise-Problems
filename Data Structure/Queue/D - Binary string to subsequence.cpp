c722f5fcd494321550e44bc021c8b97b6645895d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-11 21:20:06 +0530

    Update D - Binary string to subsequence.cpp

--- Data Structure/Queue/D - Binary string to subsequence.cpp
+++ Data Structure/Queue/D - Binary string to subsequence.cpp
@@ -5,3 +5,54 @@ Approach :- We will use two deque to store the no of subsequence , first will st
             and if s[i] == '1' then if we have any available '0' the '1' in that subseqence and update the answer too . And if we don't have any '0' available then we
             will have to add new subsequence , basically update 'd' to d+1 , the 'd' show the no of subsequence and update it in deque .
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        string s;
+        cin>>s;
+        
+        deque<int>one,zero;
+        //then find using deque the required
+        vector<int>ans(n);
+        int d = 1;
+        for(int i=0;i<n;++i){
+            if(s[i] == '1'){
+                
+                if(zero.size() == 0){
+                    ans[i] = d;
+                    one.push_back(d);
+                    d++;
+                }
+                else{
+                    ans[i] = zero.front();
+                    one.push_back(zero.front());
+                    zero.pop_front();
+                }
+            }
+            else{
+                if(one.size() == 0){
+                    ans[i] = d;
+                    zero.push_back(d);
+                    d++;
+                }
+                else{
+                    ans[i] = one.front();
+                    zero.push_back(one.front());
+                    one.pop_front();
+                }
+            }
+        }
+        
+        cout<<d-1<<"\n";
+        for(auto&x:ans)cout<<x<<" ";
+        cout<<"\n";
+    }
+}
