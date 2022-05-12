68877fb989aa864db93fad7acaaccf5b372272d5
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-12 21:18:03 +0530

    Update 47. Permutations II.cpp

--- Brute force/47. Permutations II.cpp
+++ Brute force/47. Permutations II.cpp
@@ -2,3 +2,18 @@
 Platform :- Leetcode
 Approach :- We will first sort the array and then will use built in function "next_permutaion" on do - while loop to get all the possible permutaions
 */
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>>ans;
+        
+        sort(nums.begin(),nums.end());
+        
+        do{
+            ans.push_back(nums);
+        }
+        while(next_permutation(nums.begin(),nums.end()));
+        
+        return ans;
+    }
+};
