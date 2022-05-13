5bff542eb380a30629286f57ab746f9be7330fe4
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-13 22:07:44 +0530

    Update (A) Minimums and Maximums.cpp

--- Maths/(A) Minimums and Maximums.cpp
+++ Maths/(A) Minimums and Maximums.cpp
@@ -3,3 +3,22 @@ Platform :- Codefoces
 Contest :- Codeforces Educational round 128
 Approach :- If we have overlapping interval , then answer is max of (a,c) else it is sum of (a+c_)
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        
+        int ans = 0;
+        if(c<=b && c>=a  || (a<=d && a>=c)){
+            //cout<<"* ";
+            ans = max(a,c);
+        }
+        else ans = a+c;
+        cout<<ans<<"\n";
+    }
+}
