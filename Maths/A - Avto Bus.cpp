d8cfdaf0825257d460b0893f58d45a89ab102b94
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-14 21:13:15 +0530

    Update A - Avto Bus.cpp

--- Maths/A - Avto Bus.cpp
+++ Maths/A - Avto Bus.cpp
@@ -7,3 +7,35 @@ Approach :- If we have any odd value or even value less than 4 then we cannot ha
              if we will use '6' wheel buses then we will get only '2' but we can remove one '6' wheel and get '2' 4 wheel which will 
              give us n = 14 and minimum no of buses will be '3'.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        
+        long long mn = 0,mx = 0;
+        //mn represent the minimun no of bags reqi
+        long long  rem = n%6ll;
+        if(rem%2ll){
+            cout<<"-1\n";
+            continue;
+        }
+        if(n == 2){
+            cout<<"-1\n";
+            continue;
+        }
+        
+        mn = n/6ll;
+        if(rem == 4)mn++;
+        else if(rem == 2){
+            mn--;
+            mn+=2;
+        }
+        mx = n/4ll;
+        cout<<mn<<" "<<mx<<"\n";
+    }
+}
