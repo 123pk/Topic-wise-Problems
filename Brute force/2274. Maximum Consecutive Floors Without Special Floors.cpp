f09752ababeefcd177135439b02b48cd1da5417d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-15 22:20:41 +0530

    Update 2274. Maximum Consecutive Floors Without Special Floors.cpp

--- Brute force/2274. Maximum Consecutive Floors Without Special Floors.cpp
+++ Brute force/2274. Maximum Consecutive Floors Without Special Floors.cpp
@@ -3,3 +3,20 @@ Platform :- Leetcode
 Contest :- Leetcode Weekly contest 293
 Approach :- Add the bottom and top value in array and sort in increasing order now find the max difference betweeen adjacent elements and that is our required answer 
 */
+class Solution {
+public:
+    int maxConsecutive(int bottom, int top, vector<int>& special) {
+        int ans = 0;
+        
+        special.push_back(bottom);
+        special.push_back(top);
+        sort(special.begin(),special.end());
+        
+        for(int i=1;i<special.size();++i){
+            int val = special[i]-special[i-1];
+            if(i>1 && i<special.size()-1)val--;
+            ans = max(ans,val);
+        }
+        return ans;
+    }
+};
