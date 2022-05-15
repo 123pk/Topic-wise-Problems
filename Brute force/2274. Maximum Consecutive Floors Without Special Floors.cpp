6e942e946c2d7bf4bda860c5d47ede15dc54591c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-15 22:20:22 +0530

    Create 2274. Maximum Consecutive Floors Without Special Floors.cpp

---
+++ Brute force/2274. Maximum Consecutive Floors Without Special Floors.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 293
+Approach :- Add the bottom and top value in array and sort in increasing order now find the max difference betweeen adjacent elements and that is our required answer 
+*/
