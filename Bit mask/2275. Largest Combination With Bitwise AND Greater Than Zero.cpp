f48892a1736a88c786074889bcd5e5a8070a5bfc
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-15 22:17:08 +0530

    Update 2275. Largest Combination With Bitwise AND Greater Than Zero.cpp

--- Bit mask/2275. Largest Combination With Bitwise AND Greater Than Zero.cpp
+++ Bit mask/2275. Largest Combination With Bitwise AND Greater Than Zero.cpp
@@ -3,3 +3,21 @@ Platform :- Leetcode
 Contest :- Leetcode Weekly contest 293
 Approach :- We find the frequenc of every bit and the most frequent bit (its frequency) is our required answer.
 */
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+        int ans = 0;
+        
+        vector<int>fre(32);
+        for(auto&x:candidates){
+            string temp = bitset<32>(x).to_string();
+            reverse(temp.begin(),temp.end());
+            for(int i=0;i<32 ;++i){
+                fre[i]+=(temp[i]-'0');
+                ans = max(ans,fre[i]);
+            }
+        }
+        
+        return ans;
+    }
+};
