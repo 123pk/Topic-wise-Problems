a64b24f5b71b590a1747df576017392040e280ba
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-17 22:29:43 +0530

    Update 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp

--- Tree/1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp
+++ Tree/1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp
@@ -1,4 +1,28 @@
 /*
 Platform :- Leetcode 
-Approach :- 
+Approach :- We will clone it and accordingly find the node.
 */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        if(!cloned || !original) return cloned;
+        
+        if(original == target)
+            return cloned;
+        
+        const auto left = getTargetCopy(original -> left, cloned -> left, target);
+        const auto right = getTargetCopy(original -> right, cloned -> right, target);   
+        
+        return left ? left : right;
+    }
+};
