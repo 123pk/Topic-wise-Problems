ba2d6707ed30f9f0bcea4fbd8b0363519157eacf
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-20 21:41:01 +0530

    Update 63. Unique Paths II.cpp

--- DP/63. Unique Paths II.cpp
+++ DP/63. Unique Paths II.cpp
@@ -9,3 +9,38 @@ Approach :- Since we are allowed to have two moves only ,either go to "rigth" or
             for i == 0  dp[i][j] = dp[i][j-1]
             
 */
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int ans=0;
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(i==0||j==0){
+                   if(obstacleGrid[i][j]==1)dp[i][j]=0;
+                   else{if(i==0&&j==0)dp[i][j]=1;
+                    else if(j==0)dp[i][j]=dp[i-1][j];
+                    else dp[i][j]=dp[i][j-1];
+                    }
+                }
+                else{
+                    if(obstacleGrid[i][j]==1)dp[i][j]=0;
+                    else{
+                        dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                    }
+                }
+            }
+        }
+        
+       /* for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                cout<<dp[i][j]<<" ";
+            }
+             cout<<"\n";
+        }*/
+        return dp[n-1][m-1];
+    }
+};
