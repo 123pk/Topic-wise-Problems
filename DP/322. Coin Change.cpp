98e9dd46ed7799e068cc4c80e0c07543644aa0e2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-21 21:57:01 +0530

    Create 322. Coin Change.cpp

---
+++ DP/322. Coin Change.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- It is standard Dynamic programming problem which we will use 2-d dp to solve , state of dp[i] = min(dp[i], dp[i-coins[j]] + 1 ) 
+            here coins[j] is list of all denominations we have 
+*/
