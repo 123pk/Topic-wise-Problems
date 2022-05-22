bc0087970fa80e26c92f9e9ce835389edaa3d65b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-22 21:56:26 +0530

    Create Palindromic Deletion.cpp

---
+++ Maths/Palindromic Deletion.cpp
@@ -0,0 +1,10 @@
+/*
+Platform :- Coding Competition with google
+Contest :- Google Kickstart Round C
+Approach :- So for test case one , we will try out all the possible options we have and look for palindromic substring after each operation .
+            Let 'd' be count of points we have and 'x' be total substrings then we will need to find (d/x)%mod .
+            --> You need to know modular arithmetic for this 
+                 (a/b) % mod = a* ( b^(mod-2)) % mod)  , where mod is prime
+
+Time Complexity :- O(n!)
+*/
