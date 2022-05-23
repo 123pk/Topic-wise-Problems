77ff257edf9c185e5a935be8dfa06b66e340a052
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-23 22:08:30 +0530

    Update B - Card Trick.cpp

--- Greedy/B - Card Trick.cpp
+++ Greedy/B - Card Trick.cpp
@@ -4,3 +4,34 @@ Contest :- Codeforces Educational Round 129
 Approach :- IF size of array is 'n' , then if we do n rotaition we will get back to same array , so find the sum of elements of array B, let it be sum_B,
             sum_B % = n ,  the value at the (sum_B) in array is our answer
 */
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+     int t;
+     cin>>t;
+     
+     while(t--){
+         int n;
+         cin>>n;
+         
+         int A[n];
+         for(int i=0;i<n;++i)cin>>A[i];
+         
+         int m;
+         cin>>m;
+         
+         int B[m];
+         long long tot = 0;
+         for(int i=0;i<m;++i){
+             cin>>B[i];
+             tot+=B[i];
+         }
+         
+         tot%=n;
+         cout<<A[tot]<<"\n";
+         
+          
+     }
+}
