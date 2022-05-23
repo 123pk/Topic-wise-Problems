9aa7605a5abb2d953f034276b73df08d6e004d45
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-23 22:08:15 +0530

    Create B - Card Trick.cpp

---
+++ Greedy/B - Card Trick.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Educational Round 129
+Approach :- IF size of array is 'n' , then if we do n rotaition we will get back to same array , so find the sum of elements of array B, let it be sum_B,
+            sum_B % = n ,  the value at the (sum_B) in array is our answer
+*/
