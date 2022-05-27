40923a8ec35c1300d33022874cc5c85cd979ed4d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-27 21:27:25 +0530

    Update and rename X.cpp to C. Circular Local MiniMax.cpp

---
+++ Greedy/C. Circular Local MiniMax.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 794 Div 2
+Approach :- First we will sort our first array and after that we will check if our second array is sorted or not , if it is not sorted and the elements which we need to
+            swap are distinct in array 1 we cannot sort it as if we will try to swap them then it will make array 1 unsorted .
+            else we print the indexes we need to swap .
+Runtime :- O(n log n) --> due to sorting
+*/
--- Greedy/X.cpp
+++
@@ -1,5 +0,0 @@
-/*
-Platform :- Codeforces
-Contest :- Codeforces Round 794 Div 2
-Approach :- Solution will be added once the contest is over
-*/
