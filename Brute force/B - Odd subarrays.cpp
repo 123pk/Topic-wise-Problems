c54e4f91e343cdb19d50bbb9dd4ee9af49e71acb
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-27 21:29:32 +0530

    Update and rename x.cpp to B - Odd subarrays.cpp

---
+++ Brute force/B - Odd subarrays.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 794 Div 2
+Approach :- We will count no of odd subarrays from left to right and right to left , the max out of both is our solution.
+*/
