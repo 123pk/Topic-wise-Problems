7470a21d1d94aef7a9f6d787e993399732448e4f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-28 19:34:41 +0530

    Create 1690. Stone Game VII.cpp

---
+++ DP/1690. Stone Game VII.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Approach :- We will use dp , as we have two option either to remove one element from the left or from the extreme right of available stone array .
+            we will try to get the the max difference we can get if we choose the first element or last element and we return that value .
+            
+*/
