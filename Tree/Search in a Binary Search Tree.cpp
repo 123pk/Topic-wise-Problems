37fd25cb2e7bb8f16275843e726151b7d0784777
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-30 22:45:51 +0530

    Update Search in a Binary Search Tree.cpp

--- Tree/Search in a Binary Search Tree.cpp
+++ Tree/Search in a Binary Search Tree.cpp
@@ -2,3 +2,26 @@
 Platform :- Leetcode
 Approach :- Do DFS and ass soon as we foudn the value we store in a variable and at the end we return it
 */
+class Solution {
+public:
+    TreeNode* ans ;
+    void find(TreeNode*node,int value,int& f){
+        if(f)return;
+        
+        if(!node)return ;
+        if(node->val == value){
+            f=1;
+            ans = node;
+            return;
+        }
+        find(node->left,value,f);
+        find(node->right,value,f);
+    }
+    
+    TreeNode* searchBST(TreeNode* root, int val) {
+        
+        int f = 0;
+        find(root,val,f);
+        return ans;
+    }
+};
