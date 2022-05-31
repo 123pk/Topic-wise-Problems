f84ace6c529c9170d88fd3bf16165ea3db15ed3f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-05-31 22:29:21 +0530

    Update A  - Beat the odds.cpp

--- Constructive/A  - Beat the odds.cpp
+++ Constructive/A  - Beat the odds.cpp
@@ -2,3 +2,24 @@
 Platform :- Codeforces
 Approach :- Min count of odd and even values in array 
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+         int n;
+         cin>>n;
+         
+         long long A[n];
+         int c = 0;
+         for(int i=0;i<n;++i){
+             cin>>A[i];
+             if(A[i]%2)c++;
+         }
+         int ans = min(c,n-c);
+         cout<<ans<<"\n";
+    }
+}
