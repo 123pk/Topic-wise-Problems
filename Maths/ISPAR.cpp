ff178f10be28424b5c9fec2460185fe98745c596
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-06-02 20:53:23 +0530

    Update ISPAR.cpp

--- Maths/ISPAR.cpp
+++ Maths/ISPAR.cpp
@@ -7,3 +7,24 @@ Approach :- if k == 1, then we will always get the current parity of 'n'
             
             Why , because we will always be left with 2 to k-1, values and on each move we will get rid of odd elements except of the case when k = 2 
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n,k;
+        cin>>n>>k;
+        
+        if(k == 1){
+            if(n%2ll)cout<<"ODD"<<"\n";
+            else cout<<"EVEN\n";
+        }else{
+            if(k == 2ll)cout<<"ODD\n";
+            else cout<<"EVEN\n";
+        }
+        
+    }
+}
