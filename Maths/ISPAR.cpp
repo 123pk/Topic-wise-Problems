775592e8ec6f076045a4928b26a70e063a0bb560
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-06-02 20:53:07 +0530

    Create ISPAR.cpp

---
+++ Maths/ISPAR.cpp
@@ -0,0 +1,9 @@
+/*
+Platform :- Codechef
+Contest :- Codechef Starters 41
+Approach :- if k == 1, then we will always get the current parity of 'n'
+            else for k = 2, we will always get 'ODD' parity
+            else for all other values we will get even parity.
+            
+            Why , because we will always be left with 2 to k-1, values and on each move we will get rid of odd elements except of the case when k = 2 
+*/
