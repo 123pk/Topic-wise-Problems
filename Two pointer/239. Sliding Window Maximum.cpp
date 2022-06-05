9e1cd75a5acbe887c4642caad16e79f01bc95e53
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-06-05 21:06:09 +0530

    Create 239. Sliding Window Maximum.cpp

---
+++ Two pointer/239. Sliding Window Maximum.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- We will maintain one frequency array and one set which will give us the largest value in current subarray from index [l,r] , we will use sliding window and
+            update the frequency as we go on , if frequency of element becomes zero we will erase it from the array
+*/
