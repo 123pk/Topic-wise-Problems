2ca68309fef7e3da03f08add9c15f657065a85dc
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-06-07 22:44:53 +0530

    Create C - Restoring the Duration of Tasks.cpp

---
+++ Two pointer/C - Restoring the Duration of Tasks.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 797 Div 3
+Approach :- We will look for no of black cell in window of size 'k' using sliding window, the min no of white no of cells in any window is our answer.
+*/
