c4c276c0af5d03ecb795dc30a14d9d10a46399d6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-06-09 21:58:07 +0530

    Update 167. Two Sum II - Input Array Is Sorted.cpp

--- Binary Search/167. Two Sum II - Input Array Is Sorted.cpp
+++ Binary Search/167. Two Sum II - Input Array Is Sorted.cpp
@@ -1,1 +1,40 @@
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int>ans;
+        
+         
+        //now constant space solution 
+        //we will use bitwise or operation 
+        //since both values need to be smaller than target 
+        //so will choose that element only which will get us the value
+        int f = 0;
+        for(int i=0;i<numbers.size();++i){
+             if((target-numbers[i])<numbers[i])break;
+            
+             //binary searchthis value
+            int start = i+1,end = numbers.size()-1,mid = (start+end)/2,val = target-numbers[i];
+             while(start<=end){
+                 if(numbers[mid] == val){
+                     f=1; ans = {i+1,mid+1};break;
+                    
+                 }
+                 else{
+                     if(numbers[mid]>val){
+                         end = mid-1;
+                         mid = (start+end)/2;
+                     }
+                     else {
+                         start = mid+1;
+                         mid = (start+end)/2;
+                     }
+                 }
+             }
+            
+            if(f)break;
+        }
+        
+        return ans;
+    }
+};
