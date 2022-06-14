5954789634ed981337a9300e9a03710eb12dc6f6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-06-14 22:53:56 +0530

    Create 583. Delete Operation for Two Strings.cpp

---
+++ DP/583. Delete Operation for Two Strings.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- We will use dynamic programming to find the solution , which is to find longest subsequence length and extra characters need to be removed , so final answer is
+            (len1 - len_of_longest_subs) + (len2 - len_of_longest_subs)
+*/
