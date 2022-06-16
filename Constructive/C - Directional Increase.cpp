ed842557b175ab0c1fd549e6c236b62aa87e79e7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-06-16 22:17:07 +0530

    Create C - Directional Increase.cpp

---
+++ Constructive/C - Directional Increase.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 800
+Approach :- We will check few things , the last index where non_zero value is present and will store it,
+            We will traverse through the array and if at any point of time we have our total_sum of values <= 0 , then we cannot have a valid answer/solution. 
+*/
