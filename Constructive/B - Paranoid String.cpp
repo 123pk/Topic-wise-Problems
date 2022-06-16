165e1d4d867a4bf0846e711f9a6f4eaece5f1ff9
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-06-16 22:13:04 +0530

    Update B - Paranoid String.cpp

--- Constructive/B - Paranoid String.cpp
+++ Constructive/B - Paranoid String.cpp
@@ -1,5 +1,6 @@
 /*
 Platform :- Codeforces
 Contest :- Codeforces Round 800 Div 2
-Approach :- 
+Approach :- We will traverse through the array and will maintain one count , as soon as we foound s[i] != s[i-1] , we will add count to our answer,and update it .
+           we will start with inital value of our anwer as 'n' 
 */
