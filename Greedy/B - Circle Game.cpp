a413a64d529875b1a1f64e4bfafa96009f01b182
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-06-18 22:12:19 +0530

    Create B - Circle Game.cpp

---
+++ Greedy/B - Circle Game.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 801 Div 2
+Approach :- If n is odd then , Mike will always win .
+            If n is even, then there are two cases when the smallest value that one can get in their turn is equal , then the first player to reach to smallest value
+            is looser , if the smallest values are different then the one with smaller min value looses.
+*/
