9e70d9fa4601f29b50ec16ed7f2306ef7ddd43b3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-06-21 21:43:44 +0530

    Update 1642. Furthest Building You Can Reach.cpp

--- Heap/1642. Furthest Building You Can Reach.cpp
+++ Heap/1642. Furthest Building You Can Reach.cpp
@@ -4,3 +4,27 @@ Approach :- We will maintain a max heap and we will add the amount of bricks req
             then we remove the max value of required brick till current index and will increment the count of such operation , if the count > 'k' we break out of loop.
             and the last index-1 is our answer.
 */
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        int ans = 0;
+        int n = heights.size();
+        int tot = 0,i;
+        priority_queue<int>P;
+        for(i=1;i<n;++i){
+            if(heights[i]>heights[i-1]){
+                tot+=(heights[i]-heights[i-1]);
+                P.push(heights[i]-heights[i-1]);
+            }
+    
+            if(tot>bricks){
+               if(ladders == 0)break;
+                tot -= P.top();
+                P.pop();
+                ladders--;
+            }
+        }
+        ans = i-1;
+        return ans;
+    }
+};
