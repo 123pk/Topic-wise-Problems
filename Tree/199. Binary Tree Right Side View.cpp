47135efd764d7b306014b8aa1be7b12d95b46683
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-06-21 21:39:26 +0530

    Update 199. Binary Tree Right Side View.cpp

--- Tree/199. Binary Tree Right Side View.cpp
+++ Tree/199. Binary Tree Right Side View.cpp
@@ -2,3 +2,29 @@
 Platform :- Leetcode
 Approach :- We will do level order traverl and last node value at each level is added in our answer array as all the values before it cannot be seem from right side.
 */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
+        if(!root)return ans;
+        
+        queue<TreeNode*>bfs;
+        bfs.push(root);
+        while(!bfs.empty()){
+            int c = bfs.size();
+            int node = -1;
+            for(int i=0;i<c;++i){
+                TreeNode* temp = bfs.front();
+                bfs.pop();
+                
+                node = temp->val;
+                
+                if(temp->left)bfs.push(temp->left);
+                if(temp->right)bfs.push(temp->right);
+            }
+            ans.push_back(node);
+        }
+        
+        return ans;
+    }
+};
