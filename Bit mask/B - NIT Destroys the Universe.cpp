e648bf0628a43d7922532c2e64f0a7caa19ff495
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-06-25 22:35:57 +0530

    Create B - NIT Destroys the Universe.cpp

---
+++ Bit mask/B - NIT Destroys the Universe.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Global Round 21
+Approach :- If you will analyse you will find you need atmax 2 opearation in worst case ,now you will count no of subarrays which are between zeros or zeros and bounday
+            and min (value,2) is our answer.
+*/
