894007d710a695ab0b1e77a73da73bd954628493
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-06-25 22:37:31 +0530

    Update C - Fishingprince Plays With Array.cpp

--- Maths/C - Fishingprince Plays With Array.cpp
+++ Maths/C - Fishingprince Plays With Array.cpp
@@ -4,7 +4,7 @@ Contest :- Codeforces Global Round 21
 Approach:- We will first perform operation '1' on array A till the time it is possible , we will maintain two things {value,count} , now we will traverse through array 
            B and will check if we can get the B[i] from P[j] or not . We can only get if B[i]%P[j] == 0 and the required value is power of m , req = B[i]/P[j] , req  = m^b
            else we cannot get that values
-Time Complexity = O(n+m)
+Time Complexity = O(max(n,k)
 */
 #include<bits/stdc++.h>
 using namespace std;
