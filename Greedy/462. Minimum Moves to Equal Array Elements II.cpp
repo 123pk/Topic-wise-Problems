760dc96c929c7d31ae6fbd99e209dc4a4afb0bd8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-06-30 21:08:22 +0530

    Create 462. Minimum Moves to Equal Array Elements II.cpp

---
+++ Greedy/462. Minimum Moves to Equal Array Elements II.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Appraoch :- We sort the array and it is optimal to make all elements equal to mid value of array , so we choose 5 value to left of mid and 5 to the right and find the lowest
+            cost for converting out of cost value of each elements.
+*/
