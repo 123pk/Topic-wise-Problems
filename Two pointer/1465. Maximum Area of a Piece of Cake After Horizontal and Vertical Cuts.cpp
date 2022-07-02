1214503c732cf2ff7b882e3532b5399de14a1d98
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-02 21:49:33 +0530

    Create 1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cpp

---
+++ Two pointer/1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- We find the max difference between two lines horizontally and vertically , we consider bounday as lines too , then the product of max difference gives us 
+            maximum area of rectangle
+*/
