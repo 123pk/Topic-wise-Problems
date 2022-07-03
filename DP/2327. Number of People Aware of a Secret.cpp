148bb84b93d509c4045cc9c39cc84b5972bbe48d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-03 20:57:11 +0530

    Update 2327. Number of People Aware of a Secret.cpp

--- DP/2327. Number of People Aware of a Secret.cpp
+++ DP/2327. Number of People Aware of a Secret.cpp
@@ -5,3 +5,29 @@ Approach :- A person can effect (forget - delay) people only , so we can solve t
             we can affect/share news if and only if I got the news (i-delay)days before, so from (i-delay) to j>(i-forget) , dp[i] += dp[j] ,take care of mod'
             at the end no of people effected from nth day to n-target day sum is our answr
 */
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<int>dp(n+1);
+        int  mod = 1e9+7;
+        
+        for(int i=1;i<=n;++i){
+            if(i== 1)dp[i]= 1;
+            else{
+                if(i<=delay)continue;
+                for(int j=i-(delay);j>max(-1,(i-forget));--j){
+                    dp[i] = (dp[i] + dp[j])%mod;
+                }
+            }
+        }
+     //  for(int i=1;i<=n;++i)cout<<dp[i]<<" ";
+      //  cout<<"\n";
+        
+        int ans = 0;
+        for(int i=n;i>(n-forget);--i){
+            ans = (ans+dp[i])%mod;
+        }
+        
+        return ans;
+    }
+};
