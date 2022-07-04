c1ea2d61c18d4c84fa6ddaca0caf4ffa7711d469
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-04 22:09:38 +0530

    Create B. Almost Ternary Matrix.cpp

---
+++ Constructive/B. Almost Ternary Matrix.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 804 Div 2
+Approach :- Constructive algorithm (10) start with this horizontally and vertically change it alternatingy(01) .. so after (10 -- 01 .. 10 ..01) and so on 
+           same vertically
+*/
