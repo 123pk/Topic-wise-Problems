27a7f6c3e434c9c47ac9b697119557d3cdc50d98
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-08 23:24:57 +0530

    Create C - Schedule Management.cpp

---
+++ Binary Search/C - Schedule Management.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Educational Round 131
+Approach :- We will do binary search on it , since max no of days can be 2*m , so for 'i' no of days we can find total no of tasks that can be completed by labours 
+            if tot_task_completed >= m , then we can do all the task in 'i' days , so using binary search we can do find the value.
+*/
