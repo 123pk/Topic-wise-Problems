4401c85a5efc6c0ba506a8c51c92f8a77089ae28
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-09 21:51:39 +0530

    Update 2333. Minimum Sum of Squared Difference.cpp

--- Heap/2333. Minimum Sum of Squared Difference.cpp
+++ Heap/2333. Minimum Sum of Squared Difference.cpp
@@ -13,3 +13,79 @@ Approach :- Find the abs differece of values abs(nums1[i] - nums2[i]) and store 
  Time Complexity :- O(n*log(n))
  Space complexity :- O(n)
 */
+class Solution {
+public:
+    long long minSumSquareDiff(vector<int>& nums1, vector<int>& nums2, int k1, int k2) {
+        long long ans = 0;
+        
+        long long val = k1+k2;
+        priority_queue<long long>P;
+        
+        map<long long,long long>count;
+        int n = nums1.size();
+        for(int i=0;i<n;++i){
+            count[abs(nums1[i] -nums2[i])]++;
+        }
+        
+        for(auto&x:count){
+            P.push(x.first);
+        }
+        
+        while(!P.empty() && val){
+            long long first = P.top();
+            P.pop();
+            long long c =count[first];
+            //cout<<first <<" "<<c<<"\n";
+            if(!P.empty()){
+                //we will get the second smallest value
+                long long second = P.top();
+                long long dif = first - second;
+                
+                dif*=c;
+                
+                if(dif>val){
+                    //this  means we cannot covert all the values to smaller value
+                    //so we will convert to some mid value
+                    long long rem = val%c;
+                    long long change = val/c;
+                    long long nw = first - change;
+                    count[nw] += c - rem;
+                    count[nw-1] += rem;
+                    count[first] -= c;
+                    break;
+                }
+                else{
+                    //we change them to second smallest value
+                    val -= dif;
+                    count[second]+=c;
+                    count[first] -= c;
+                }
+            }
+            else{
+                //if we have justs one value we will try to chagne this to as small as possible
+                //first we will check if we can convert to 0 or not
+                long long cost = first *c;
+                if(cost<=val){
+                    count[first] = 0;
+                    break;
+                }
+                long long rem = val%c;
+                long long change = val/c;
+                long long nw = first - change;
+                count[nw] += c - rem;
+                count[nw-1] += rem;
+                count[first] -= c;
+                break;
+            }
+        }
+        
+        for(auto&x:count){
+            long long val = x.first;
+            val*=val;
+            val*=x.second;
+            ans+=val;
+        }
+        
+        return ans;
+    }
+};
