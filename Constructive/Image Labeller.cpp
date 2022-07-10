30439f303ee52d9ce1fc9d7ae11a123e8a4a7905
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-10 23:15:32 +0530

    Create Image Labeller.cpp

---
+++ Constructive/Image Labeller.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Coding Competition with google
+Contest :- Google Kickstart Round D 2022
+Approach :- It is optimal to sort the array and make set of size '1' of largest (m-1) values and for last set of (n-(m-1)) values we will get the middle value as said in 
+            problem statement
+Time Complexity :- O(n)
+*/
