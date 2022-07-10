6a345a52e898e47092d62538e4a482f12b73ba41
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-10 23:20:28 +0530

    Create Maximum Gain.cpp

---
+++ Two pointer/Maximum Gain.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Coding Competition with google
+Contest :- Google Kickstart Round D 2022
+Approach :- We can have 'k' moves so we will try to divide the moves optimally such that we get maximum sum , for each 'i' from 0 to 'k' we will do (i) operations from first
+            array and min(m,k-i) operations from array 2 , in those moves we will find the max value we can acheive and max of sum of all those value pair is our solution. 
+*/
