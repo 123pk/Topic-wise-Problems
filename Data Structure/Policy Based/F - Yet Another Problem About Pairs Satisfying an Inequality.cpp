c1f86a45e2fdd1992bf74ad5a6621dda791e0f62
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-12 22:36:49 +0530

    Create F - Yet Another Problem About Pairs Satisfying an Inequality.cpp

---
+++ Data Structure/Policy Based/F - Yet Another Problem About Pairs Satisfying an Inequality.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 806 Div 4
+Appraoch :- Ordered set use to find number of values which are strictly greater tahn current 'i' value to its right . We make seperate array of value pairs which for
+            or satisfy A[i]<i
+*/
