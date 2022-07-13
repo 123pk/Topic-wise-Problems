03f24499eba9fa76051ee95cb6ab1844a4ec3ff4
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-13 22:43:19 +0530

    Create E - Split in two sets.cpp

---
+++ Graph/E - Split in two sets.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 805 Div 3
+Approach :- We will make graph of the points and if there is cycle of odd edge length then we cannot seperatte them out , or if we have same elements in pair,or 
+            if frequency of some element is >2
+*/
