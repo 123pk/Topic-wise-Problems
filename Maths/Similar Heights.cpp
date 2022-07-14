c59ba355be05c86c869e0e7976926d4f64dcd691
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-14 22:02:04 +0530

    Update Similar Heights.cpp

--- Maths/Similar Heights.cpp
+++ Maths/Similar Heights.cpp
@@ -7,3 +7,47 @@ Appraoch :- You need to carefully understand the lines in problem statement
            --> if c == 1 and the max_size of any gropu is  = 2 then we have to do '2' opeartion
            --> else we need '1' operation and this is our edge case
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        map<long long ,int>P;
+        long long A[n];
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            P[A[i]]++;
+        }
+        
+        int ans = 0;
+        int c = 0;
+        int mx = 0;
+        long long val = -1;
+        for(auto&x:P){
+            mx= max(mx,x.second);
+            if(x.second == 1){
+                c++;
+                val = x.first;
+            }
+        }
+        
+        sort(A,A+n);
+        
+        if(c == 1){
+            if(mx == 2){
+                if(val == A[n-1])ans+=2;
+                else ans++;
+            }
+            else ans++;
+        }
+        else ans += (c+1)/2;
+         
+        cout<<ans<<"\n";
+    }
+}
