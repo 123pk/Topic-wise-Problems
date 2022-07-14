24925298b7588226a8f0bcec8856fa7dd91eac87
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-14 22:01:51 +0530

    Update Similar Heights.cpp

--- Maths/Similar Heights.cpp
+++ Maths/Similar Heights.cpp
@@ -1,5 +1,9 @@
 /*
 Platform :- Codechef
 Contest :- Codechef Starters 47
-Appraoch :- 
+Appraoch :- You need to carefully understand the lines in problem statement 
+           --> You can sepearate out all the group of size  = 1 , let c = count of such values then 
+           --> if c>1 then it will cost us (c*(c+1))/2 operations to make them into a single group and it will be the minimum cost too
+           --> if c == 1 and the max_size of any gropu is  = 2 then we have to do '2' opeartion
+           --> else we need '1' operation and this is our edge case
 */
