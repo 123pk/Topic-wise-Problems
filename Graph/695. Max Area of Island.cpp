5ae1f0d26b26506f0bc6cfada4063fa5ea16dc45
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-15 21:22:19 +0530

    Create 695. Max Area of Island.cpp

---
+++ Graph/695. Max Area of Island.cpp
@@ -0,0 +1,4 @@
+/*
+Platform :- Leetcode
+Approach :- We do bfs from each of the long as long we can go and count the area , we donnot visit visited areas and water , we maintain onoe visited 2d array for that.
+*/
