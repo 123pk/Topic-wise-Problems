7070c04e7eadc2b5b9a5894cb3cc416469e8d4ba
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-18 22:10:29 +0530

    Create D1 - Chopping Carrots (Easy Version).cpp

---
+++ Greedy/D1 - Chopping Carrots (Easy Version).cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 809 Div 2
+Approach :- Since the constraints are small , we will try to covert each value in array to nearest possible value of 'i' for all 'i' from '1' to 3000 , solution is 
+            the min difference between the max and min value near to 'i'
+*/
