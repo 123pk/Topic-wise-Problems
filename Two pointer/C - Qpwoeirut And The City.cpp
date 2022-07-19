9f995355d3721f8017fa6b7e07f950978322c4cb
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-19 22:27:08 +0530

    Create C - Qpwoeirut And The City.cpp

---
+++ Two pointer/C - Qpwoeirut And The City.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 809 Div 2
+Approach :- For odd length of 'n' we have one solution and that is to make all values at even index (1-based indexing) greater than their adjacne neighbours.
+            For even length of 'n' we have three options and min cost of those three is our solution
+Time Complexity :- O(n)
+*/
