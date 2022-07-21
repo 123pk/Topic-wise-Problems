4b01fec743f26d5896c3e6566c8b5b22923e8504
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-21 17:20:04 +0530

    Update TREEXOR.cpp

--- Tree/TREEXOR.cpp
+++ Tree/TREEXOR.cpp
@@ -1,1 +1,6 @@
-
+/*
+Platform :- Codechef
+Contest :- Codechef starters 48 Div 2
+Approach :- We will first sort the nodes by level where root is at level '1' and leaf is at 'd' where 'd' is the depth of tree . Now we will sort nodes by decreasing order
+            of their level and assign first 'k' values with '1' , now we will make sure than all the nodes will assigned in such a way that the xor value of their subtree is '0'
+*/
