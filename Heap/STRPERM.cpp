d026859b8d93078911e2b2847526378b626da9d3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-21 17:47:12 +0530

    Update STRPERM.cpp

--- Heap/STRPERM.cpp
+++ Heap/STRPERM.cpp
@@ -7,3 +7,94 @@ Approach :- We will maintain list for each value from '1' to 'n' and store the v
             --> Solution is not possible for case where our heap or set becomes empty and we have some index to fill.
 Time Complexity :- O(n * log(n)) 
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,m;
+        cin>>n>>m;
+        
+        set<int>avail;
+        for(int i=1;i<=n;++i)avail.insert(-i);
+        
+        vector<set<int>>P(n+1);
+        vector<int>count(n+1);
+        vector<int>zz;
+        for(int i=1;i<=m;++i){
+            int val,idx;
+            cin>>val>>idx;
+            if(P[idx].size() == 0)count.push_back(idx);
+            P[idx].insert(-val);
+            
+            if(avail.find(-val)!=avail.end())avail.erase(-val);
+        }
+        int sum = 0,f = 0;
+        
+        sort(count.begin(),count.end());
+        
+        for(int i=0;i<count.size();++i){
+            
+            sum += P[count[i]].size();
+            if(sum>count[i]){
+                f=1;
+                break;
+            }
+        }
+        
+        if(f){
+            cout<<"-1\n";
+            continue;
+        }
+        int m1 = count.size();
+        if(count[m1-1] != n){
+            count.push_back(n);
+            m1++;
+        } 
+        
+        for(auto&x:avail)P[n].insert(x);
+        avail.clear();
+        
+        int i = n;
+        set<int>temp;
+        vector<int>ans;
+        int j = m1-1;
+        
+        while(i>=1){
+            //we will move and assign values
+            if(i == count[j]){
+                
+                //what will we do 
+                //we will fill the values in set
+               
+                for(auto&x:P[i])avail.insert(x);
+                
+                //store the max value availabe to current index
+                auto it = avail.begin();
+                ans.push_back(abs(*it));
+                avail.erase(it);
+                j--;
+                
+            }
+            else{
+                auto it = avail.begin();
+                ans.push_back(abs(*it));
+                avail.erase(it);
+            }
+            i--;
+            
+        }
+        
+        reverse(ans.begin(),ans.end());
+        
+        for(auto&x:ans)cout<<x<<" ";
+        cout<<"\n";
+        
+        
+        
+    }
+}
