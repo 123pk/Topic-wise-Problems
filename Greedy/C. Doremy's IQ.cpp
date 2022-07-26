5fd98ffdef23cafd31ca610e64496855895c8961
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-26 22:09:46 +0530

    Create C. Doremy's IQ.cpp

---
+++ Greedy/C. Doremy's IQ.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 808 Div 2
+Approach :- It is optimal to choose the value from end , we maintain one prefix array which store number of values (A[i] <= k) till 'ith' index, now we start
+           from the last value and try to get as many problem as we can from 'k' value.
+           
+*/
