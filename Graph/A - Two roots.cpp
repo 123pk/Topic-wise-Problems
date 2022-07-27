794b4faf10f9fb27f278fe51a2bce048ff59e555
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-27 22:28:05 +0530

    Create A - Two roots.cpp

---
+++ Graph/A - Two roots.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 333 Div 1 
+Approach :- IF you will analyse you will find that shortest path of one of medium (railways , road) is 1 as if there is no track from (1-->n) then there will surely be one
+            road from (1-->n) and vice versa . 
+            Now problem comes donw to finding the smallest path from 1 to n in other medium , if we cannot reach to n then it is '-1' else it is that path len our solution
+Time complexity :- O(V+E)
+*/
