bfa6370d33070159abacf2901adf26acd24419d8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-28 22:16:44 +0530

    Create F1-Spanning Tree with Maximum Degree.cpp

---
+++ Graph/F1-Spanning Tree with Maximum Degree.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 544 Div 3
+Approach :- find the node with max degree , now we will make that node as root, now we will find the spanning tree and that is our required solution.
+Time Complexity :- O(V+E)
+*/
