54a0b95c15c51c40f30fb27b5a4e9f174092c799
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-28 22:24:08 +0530

    Create E - Cover it !.cpp

---
+++ Graph/E - Cover it !.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 565 Div 3
+Approach :- We choose any node and find one end of diameter ( farthest node from this choosen node is one end of diameter) . Now we make that node as root and do bfs
+            from this and we find no of nodes in (odd level) and in (even level) . So the min of no of (odd level nodes and even level nodes) is choosen and marked
+*/
