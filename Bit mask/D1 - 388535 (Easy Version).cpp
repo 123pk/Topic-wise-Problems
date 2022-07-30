f5a9b2f1b72ad129f578de5edf7aadbd3e7456e0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-30 21:36:58 +0530

    Create D1 - 388535 (Easy Version).cpp

---
+++ Bit mask/D1 - 388535 (Easy Version).cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 779 Div 2
+Approach :- We will maintian one frequency array and will store the frequency of each bit in array for values from (0 to r) , now we create another frequecy array
+            where we store the frequency of bit of array given , now we compare the bits of both arrays and if they are not equal we will add 2^i value where 'i' is ith bit
+            as that bit is altered.
+*/
