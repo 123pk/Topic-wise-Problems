433afd647792ef6925c931ed5403b4ffe99b5eae
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-07-31 11:55:47 +0530

    Update 2360. Longest Cycle in a Graph.cpp

--- Graph/2360. Longest Cycle in a Graph.cpp
+++ Graph/2360. Longest Cycle in a Graph.cpp
@@ -9,3 +9,66 @@ Approach :- We will traverse from 0 to n-1, if the nodes are already visited we 
 Time complexity :- O(n)
 Space complexity :- O(n)
 */
+class Solution {
+public:
+    
+    
+    int longestCycle(vector<int>& edges) {
+        int n = edges.size();
+        vector<vector<int>>graph(n);
+        
+        
+        for(int i=0;i<n;++i){
+            if(edges[i] == -1)continue;
+            graph[i].push_back(edges[i]);
+        }
+        
+        int ans = -1;
+        vector<int>again(n);
+        vector<int>len(n);
+        for(int i=0;i<n;++i){
+            if(again[i])continue;
+            
+            queue<pair<int,int>>bfs;
+            bfs.push({i,1});
+            len[i] = 1;
+            again[i] = i+1;
+            int f = 0,val = -1;
+            
+            while(!bfs.empty()){
+                int c = bfs.size();
+                for(int j=0;j<c;++j){
+                    pair<int,int>temp = bfs.front();
+                    bfs.pop();
+                    int node = temp.first;
+                    int dist = temp.second;
+                    //cout<<node<<" -- "<<dist<<"\n";
+                    for(auto&x:graph[node]){
+                        //cout<<x<<" "<<again[x]<<" "<<(i+1)<<" \n";
+                        if(again[x]){
+                            if(again[x]!=(i+1)){
+                               f=1;break;
+                            }
+                            val = max(val,dist - (len[x]-1));
+                            continue;
+                        }
+                        again[x] = i+1;
+                        len[x] = dist+1;
+                        bfs.push({x,dist+1});
+                    }
+                //    cout<<"\n";
+                    if(f)break;
+                }
+                if(f)break;
+            }
+            //cout<<"\n";
+         //   cout<<(i+1)<<" "<<val<<"\n";
+            
+            
+            if(f == 0)ans = max(ans,val);
+        }
+        
+        
+        return ans;
+    }
+};
