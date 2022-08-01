34fd276b8852c09b07efa9024090ef1bf867c3f8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-08-01 23:17:31 +0530

    Update E - Add modulo 10.cpp

--- Maths/E - Add modulo 10.cpp
+++ Maths/E - Add modulo 10.cpp
@@ -13,3 +13,87 @@ Approach :- You need to carefully observe few things , for all values except (5 
             for 0.
             So we will do check them separately 
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        long long A[n];
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+        }
+        sort(A,A+n);
+        
+        if((A[0] == A[n-1]) || (n ==1))cout<<"Yes\n";
+        else{
+            int d = 0;
+            for(auto&x:A){
+                if(x%10 == 0){
+                    d++;
+                }
+                if(x%10 == 5){
+                    d++;
+                }
+            }
+            if(d>0 && d<n){
+                cout<<"No\n";
+                continue;
+            }
+            
+            if(d == n){
+                //we will convert each value to 10
+                for(auto&x:A){
+                    if(x%10 == 5)x+=5;
+                }
+                
+                sort(A,A+n);
+                if(A[0] == A[n-1]){
+                    cout<<"Yes\n";
+                }
+                else cout<<"No\n";
+                continue;
+            }
+            
+            int f = 0;
+            vector<long long>temp;
+            for(int k=2;k<=8;k+=2){
+                //we will convert each elemetn to have the last digit as 2
+                temp.clear();
+                for(auto&x:A)temp.push_back(x);
+                
+                for(int i=0;i<n;++i){
+                     //we will conert last digit to 2
+                    // the k
+                    while(1){
+                        long long rem = temp[i]%10ll;
+                        if(rem == k)break;
+                        temp[i]+=rem;
+                    }
+                }
+                int h = 0;
+                
+                for(int i=1;i<n;++i){
+                    long long dif = abs(temp[i]-temp[i-1]);
+                    if(dif%20){
+                        h = 1;
+                        break;
+                    }
+                }
+                if(h == 0){
+                    f=1;
+                    break;
+                }
+            }
+            
+            if(f)cout<<"Yes\n";
+            else cout<<"No\n";
+        }
+        
+    }
+}
