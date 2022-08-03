f8d0043561c86f75873f3875a1372642b99cbc87
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-08-03 22:19:49 +0530

    Create D - Fixed Point Guessing.cpp

---
+++ Binary Search/D - Fixed Point Guessing.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 803 Div 2
+Approach :- We will definitely use binary search as ( n < 10000) and no of queries is atmax (15) , now we want to know how to check if an range [l,r] contain 
+            the fixed point or not , so the range will contain fixed point if count = number of values in range are (l<=x <=r) if this count is odd.
+*/
