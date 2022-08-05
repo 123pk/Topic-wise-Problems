c54c0cfaecc01c487bbe8289d31ee4afa7501b80
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-08-05 08:57:13 +0530

    Update A- (2-3)moves.cpp

--- Maths/A- (2-3)moves.cpp
+++ Maths/A- (2-3)moves.cpp
@@ -5,3 +5,29 @@ Approach :- We first convert n to abs(n) as its easy to deal with +ve value , no
             if there is some remainder we need +1 additional jump .
             Take care of case when n == 1, here we need 2 jumps.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n;
+        cin>>n;
+        n = abs(n);
+        //we can move +3,+2 or -3,-2
+        long long ans = n/3ll;
+        long long dis = ans*3ll;
+        n-=dis;
+        if(n == 2){
+            ans++;
+        }
+        else if(n == 1){
+            if(ans)ans++;
+            else ans+=2;
+        }
+        cout<<ans<<"\n";
+        
+    }
+}
