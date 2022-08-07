7a4c9dda70ad6aa7629e539f3d292d5dbdb5a54c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-08-07 18:00:00 +0530

    Create 1220. Count Vowels Permutation.cpp

---
+++ DP/1220. Count Vowels Permutation.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Appraoch :- We will use bottom up do here , starting from nth index and filling upto 1st index , we will take care of conditions.
+Time Complexity :- O(n*k)
+*/
