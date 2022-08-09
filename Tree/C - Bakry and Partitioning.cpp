ac9795b2a3f2f75fb7af93d2cf564e3a28fd9cb2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-08-09 22:28:03 +0530

    Create C - Bakry and Partitioning.cpp

---
+++ Tree/C - Bakry and Partitioning.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 746 Div 2
+Approach :- Find the xor of all the values of array and if the xor value is '0' then we always have answer,
+            else we need to find two nodes which have subtree equal to 'x'
+*/
