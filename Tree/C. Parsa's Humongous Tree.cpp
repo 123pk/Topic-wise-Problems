516ecf50638a48077399d49c8347ef880c2896c7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-08-11 22:40:21 +0530

    Update C. Parsa's Humongous Tree.cpp

--- Tree/C. Parsa's Humongous Tree.cpp
+++ Tree/C. Parsa's Humongous Tree.cpp
@@ -1,1 +1,5 @@
-
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 722 Div 2
+Approach :- We do dfs and while coming back from leaf we check what will happen if we choose [l] and [r] from {l,r} It is optimal to chose the bounday points .
+*/
