d8137aa89c2bd35d2d48fbb7cadf0e6129fa6e46
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-08-13 22:50:16 +0530

    Update 1312. Minimum Insertion Steps to Make a String Palindrome.cpp

--- DP/1312. Minimum Insertion Steps to Make a String Palindrome.cpp
+++ DP/1312. Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -6,3 +6,31 @@ Approach :- We will maintain two pointers one will point to the end and one to t
              --- or add character similar to (jth) character in front 
              we will check for both operation and consider the one with minimum cost ,we will use memoisation to make our code more efficient.
 */
+class Solution {
+public:
+    int dp[501][501];
+    
+    int find(int i,int j,string& s){
+        if(i >= j)return 0;
+        if(dp[i][j] != -1)return dp[i][j];
+        
+        //here we have i < j
+        //so we have two option either to add one element to make i equal or add one 
+        //to make jth character of s equal
+        //what is current values are eual we move to next
+        if(s[i] == s[j]){
+            return dp[i][j] = find(i+1,j-1,s);
+        }
+        else{
+            int val = min(1 + find(i+1,j,s),1+find(i,j-1,s));
+            return dp[i][j] = val;
+        }
+        
+    }
+    
+    int minInsertions(string s) {
+        memset(dp,-1,sizeof(dp));
+        int n = s.size();
+        return find(0,n-1,s);
+    }
+};
