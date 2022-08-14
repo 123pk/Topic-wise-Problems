ef0e07e08c870de17fb9073eb432917eadb95475
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-08-14 22:24:25 +0530

    Update D. Armchairs.cpp

--- DP/D. Armchairs.cpp
+++ DP/D. Armchairs.cpp
@@ -3,3 +3,44 @@ Platform :- Codeforces
 Contest :- Codeforces Educational Round 109
 Approach :- 
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+long long dp[5001][5001];
+
+long long find(int i,int j,vector<int>&A,vector<int>&B){
+    
+    if(i>=A.size())return 0;
+    if(j>=B.size())return 1e18;
+    
+    if(dp[i][j] != -1)return dp[i][j];
+    
+    //what option we have ,either to skip the value or untilise it 
+    long long z = min(find(i+1,j+1,A,B)+abs(A[i]-B[j]),find(i,j+1,A,B));
+    
+    return dp[i][j] = z;
+    
+}
+
+int main(){
+    int t = 1;
+    //cin>>t;
+    
+    while(t--){
+          int n;
+          cin>>n;
+          
+          int A[n];
+          vector<int>C,D;
+          for(int i=0;i<n;++i){
+              cin>>A[i];
+              if(A[i])C.push_back(i);
+              else D.push_back(i);
+          }
+          
+          memset(dp,-1,sizeof(dp));
+          long long ans = find(0,0,C,D);
+          cout<<ans<<"\n";
+          
+    }
+}
