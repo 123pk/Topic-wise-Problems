ff6e0cd23e7e7e95af9293247f0163bab1ed8b03
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-08-15 22:00:56 +0530

    Update G - Good Key, Bad Key.cpp

--- DP/G - Good Key, Bad Key.cpp
+++ DP/G - Good Key, Bad Key.cpp
@@ -7,3 +7,38 @@ Approach :- We know the max value of A[i] is 10^9 , so it means if no of 2nd ope
             
 Time Complexity :- O(n*32) -- O(n)
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+ 
+        cin>>n>>k;
+        
+        vector<long long>A(n);
+        for(int i=0;i<n;++i)cin>>A[i];
+        
+        long long ans = -1e18;
+        
+        long long tot = 0;
+        for(int i=-1;i<n;++i){
+            //tot+= (A[i]-k)
+            long long sum = tot;
+            for(int j=i+1;j<min(n,i+32);++j){
+                long long z = A[j];
+                z>>=(j-i);
+                sum += z;
+            }
+            //cout<<sum<<" "<<i<<"\n";
+            ans = max(ans,sum);
+            if((i+1)<n)tot += (A[i+1]-k);
+        }
+        
+        cout<<ans<<"\n";
+    }
+}
