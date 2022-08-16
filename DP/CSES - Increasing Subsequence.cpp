16a511d3efe07021c1f038b9203a88f973c40548
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-08-16 22:25:56 +0530

    Update CSES - Increasing Subsequence.cpp

--- DP/CSES - Increasing Subsequence.cpp
+++ DP/CSES - Increasing Subsequence.cpp
@@ -8,3 +8,68 @@ Approach :- We maintain a set where we store the element of LIS , now when we ge
             So for finding just greater element we use "Lower_bound" in c++ set stl
 Time Complexity :- O(n*log(n))
 */
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+int main(){
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        vector<long long>A(n);
+        for(int i = 0;i<n;++i){
+            cin>>A[i];
+        }
+        
+        set<long long>values;
+        map<long long,int>idx;
+        int ans = 0;
+        vector<int>dp(n,1);
+        
+        
+        for(int i=0;i<n;++i){
+            if(i == 0){
+               dp[0] = 1;
+               values.insert(A[i]);
+               idx[A[i]] = 0;
+               ans = max(ans,dp[i]);
+            }   
+            else{
+              auto it = values.lower_bound(A[i]);
+              if(it == values.end()){
+                  //ther is no value that is greatear than or equal to this value
+                  //so we simplifyy add it to the lIS till this index
+                  dp[i] = ans+1;
+                  ans++;
+                  values.insert(A[i]);
+                  idx[A[i]] = i;
+                  continue;
+              }
+              
+              long long cur = *it;
+              //cout<<A[i]<<" "<<cur<<" "<<dp[idx[cur]]<<"\n";
+              dp[i] = dp[idx[cur]];
+              
+              if(A[i] == cur)continue;
+              
+              if(A[i]<cur)values.erase(cur);
+              values.insert(A[i]);
+              idx[A[i]] = i;
+              
+               
+              ans = max(ans,dp[i]);
+              
+               
+            }
+        }
+        
+        //for(int i=0;i<n;++i)cout<<dp[i]<<" ";
+      //  cout<<"\n";
+        
+        cout<<ans;
+    }
+}
