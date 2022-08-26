c9f453d54587dd5345054c1583fa2d313b3e77ce
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-08-26 23:29:52 +0530

    Create 869. Reordered Power of 2.cpp

---
+++ Brute force/869. Reordered Power of 2.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Approach :- We can convert the current number into string and try out all the permutaion of the string and if there is some string which satisfy the given condtion, 
+            lik we conver the string to integer and if the integer contains only one set bit in binary reprensentaion then it is power of 2
+Time Complexity :- O(2^n)
+*/
