be49a6ab2d8b4f23527c628d2b7286603b06be0d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-08-26 23:34:51 +0530

    Update 869. Reordered Power of 2.cpp

--- Brute force/869. Reordered Power of 2.cpp
+++ Brute force/869. Reordered Power of 2.cpp
@@ -4,3 +4,22 @@ Approach :- We can convert the current number into string and try out all the pe
             lik we conver the string to integer and if the integer contains only one set bit in binary reprensentaion then it is power of 2
 Time Complexity :- O(2^n)
 */
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        
+        string s=to_string(n);
+        int f=0;
+        sort(s.begin(),s.end());
+        do{
+            if(s[0]!='0'){
+           int m=stoi(s);
+           string z=bitset<64>(m).to_string();
+            if(count(z.begin(),z.end(),'1')==1){
+                f=1;break;
+            }
+            }
+        }while(next_permutation(s.begin(),s.end()));
+        return f;
+    }
+};
