907b0a4c9d95a52d041272f17c37c734a6939031
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-08-27 23:07:52 +0530

    Update B - Deadly Laser.cpp

--- Maths/B - Deadly Laser.cpp
+++ Maths/B - Deadly Laser.cpp
@@ -10,3 +10,29 @@ Approach :- We will check for blockage , there are four kind of blockage ,if we 
             3. Upper cut reverse ' __|' blockage
             4. lower cur r blockage 
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,m,x,y,d;
+        cin>>n>>m>>x>>y>>d;
+        
+        //now check if we are blocked or not 
+        int c = 0;
+        
+        //if left is touched
+         
+         if((n-x)<=d && (m-y)<=d)c=1;
+         if((n-x)<=d && (x-1)<=d)c=1;
+         if((y-1)<=d && (x-1)<=d)c = 1;
+         if((y-1)<=d && (m-y)<=d)c=1;
+         
+        if(c)cout<<"-1\n";
+        else cout<<(n+m-2)<<"\n";
+    
+    }
+}
