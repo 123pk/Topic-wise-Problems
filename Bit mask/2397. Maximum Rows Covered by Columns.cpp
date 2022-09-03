45dc6aff438cb7f9ddf0a042952b634f4e89369f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-03 23:14:05 +0530

    Create 2397. Maximum Rows Covered by Columns.cpp

---
+++ Bit mask/2397. Maximum Rows Covered by Columns.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leecode
+Contest :- Leetcode Bi Weekly contest 86
+Approach :- To get all the combination of rows using bitmask and then validtion of condition
+Time Complexity :- O(2^n)
+*/
