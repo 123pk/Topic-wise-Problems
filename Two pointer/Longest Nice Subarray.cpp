e30be7a8378ff15c03c1300b2803172229ce5fc0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-04 22:33:22 +0530

    Create Longest Nice Subarray.cpp

---
+++ Two pointer/Longest Nice Subarray.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 309
+Approach :- We will use one frequency array of size 32 , where we will store frequencey of each bit , we have '&' value zero if each bit do not occur more than once 
+            if it happens then we move our left/start pointer which will intitally start from '0' , start will reprensent the starting point of our subarray we keep track
+            of size of subarray too .
+Time Complexity :- O(n)
+*/
