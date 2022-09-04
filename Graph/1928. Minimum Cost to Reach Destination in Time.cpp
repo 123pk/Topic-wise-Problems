7417dbb2a6a9ea129429a1d34fbfc2609fb2eefb
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-04 22:34:10 +0530

    Update 1928. Minimum Cost to Reach Destination in Time.cpp

--- Graph/1928. Minimum Cost to Reach Destination in Time.cpp
+++ Graph/1928. Minimum Cost to Reach Destination in Time.cpp
@@ -5,6 +5,7 @@ Approach :- We will use dijkstra algorithms with priority on minimum cost/fees p
             We will move along the path if the 'time_take <= maxTime' , 
             -- Then we will check for cost , if cost to reach node 'x' is less than already stored value to reach there then we update it and do traversal again
             --- Else we look for minimum time to reach current node if that can be modified we again take it in consideration and do traversal again
+Time Complexity :- O(n*log n)
 */
 class Solution {
 public:
