409f9167a4062a3c889e86f6327338c494f182a7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-05 21:55:29 +0530

    Create 23. Merge k Sorted Lists.cpp

---
+++ Linked list/23. Merge k Sorted Lists.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- We use min heap of pair storing the value of first element of of each of k lists , now we move the pointer by taking the top element and adding it to the list
+            at the end we return the head
+*/
