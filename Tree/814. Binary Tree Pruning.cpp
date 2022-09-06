a4d706d00498fe36ab0b7457c6765af193a490a9
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-06 23:00:18 +0530

    Update 814. Binary Tree Pruning.cpp

--- Tree/814. Binary Tree Pruning.cpp
+++ Tree/814. Binary Tree Pruning.cpp
@@ -5,3 +5,48 @@ Apporach :- We will do dfs and will take care from where we get the '1' ( in lef
             
 Time Complexity :- O(n) --> n = number of edges
 */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool find(TreeNode*node){
+        if(!node)return false;
+        int f = 0;
+        int g = 0;
+        if(find(node->left))f =1;
+        if(find(node->right))g = 1;
+        if(f||g){
+            if(f == 0 )node->left = NULL;
+            if(g == 0)node->right = NULL;
+            return true;
+        }
+        else {
+            if(node->val == 1){
+                
+                node->left = NULL;
+                node->right = NULL;
+                return true;
+            }
+            return false;
+        }
+    }
+    TreeNode* pruneTree(TreeNode* root) {
+        
+        if(!root)return NULL;
+        
+        if(find(root)){
+            return root;
+        }
+        return NULL;
+    }
+};
