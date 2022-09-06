3123bf95d67f82aeebe787785aae202fbc95f450
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-06 23:00:04 +0530

    Create 814. Binary Tree Pruning.cpp

---
+++ Tree/814. Binary Tree Pruning.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Leetcode
+Apporach :- We will do dfs and will take care from where we get the '1' ( in left subtree or right) ,  we remove the edge which doesnot give us '1' or which does not
+            contain '1' (left child or right child edge).
+            
+Time Complexity :- O(n) --> n = number of edges
+*/
