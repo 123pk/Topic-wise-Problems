cbf32239cad857fb1ba0008a399ce367d6ef92d6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-08 22:11:18 +0530

    Create 371. Sum of Two Integers.cpp

---
+++ Bit mask/371. Sum of Two Integers.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- We will first find the binary string of both values ,  now we will find anther string which will be 'or' of both strings and one will be 'and' of both string
+            then we will do bit addition operaion . with carray .
+*/
