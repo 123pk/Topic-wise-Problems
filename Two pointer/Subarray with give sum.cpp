d1dbee54eda510417ad2590fef5023635f796ead
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-13 21:36:12 +0530

    Update Subarray with give sum.cpp

--- Two pointer/Subarray with give sum.cpp
+++ Two pointer/Subarray with give sum.cpp
@@ -5,3 +5,28 @@ Approach :- We maintain two pointer pointing to stat and end of required range ,
             
 Time Complexity :- O(n)
 */
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(int arr[], int n, long long s)
+    {
+        // Your code here
+        int start = 0,end = -1;
+        long long tot = 0;
+        for(int i=0;i<n;++i){
+            tot += arr[i];
+            while(tot>s){
+                tot -= arr[start];
+                start++;
+            }
+            
+            if(tot == s){
+                end = i;
+                break;
+            }
+        }
+        if(end == -1 || start>end)return {-1};
+        return {start+1,end+1};
+    }
+};
