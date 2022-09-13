e7f9835eb39c6617bf3847af1ff193073ea26bc7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-13 21:24:36 +0530

    Create Trapping Rain Water.cpp

---
+++ Stack/Trapping Rain Water.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Geeksforgeeks
+Approach :- We can trap water in non-decresasing subsequence from (0th index to 'last_max_elemet') and non_increasing sequence from (n-1 to max_element)we also maintain
+           vis array to store the index which are already being used.
+           We will use stack to find non- increasing and non_decreasing subsequence.
+Time Complexity :- O(N)
+*/
