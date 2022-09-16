45e4182e43c4301f0d75c7e896a7c22b7207a08d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-16 12:27:52 +0530

    Create 1691. Maximum Height by Stacking Cuboids.cpp

---
+++ DP/1691. Maximum Height by Stacking Cuboids.cpp
@@ -0,0 +1,9 @@
+/*
+Platform :- Leetcode
+Qualtiy :- Very Good
+Approach :- We first sort the dimennsions of array in increasing order as we can rotate in any direction and can make any type of box,
+            now we sort the values off finaly array in increasing order .
+            If you can broadly see then this problem is now reduced down to finding the LIS of the 2-D array .
+            So we use Dp to find LIS in O(n^2)
+Time Complexity :- O(n^2)
+*/
