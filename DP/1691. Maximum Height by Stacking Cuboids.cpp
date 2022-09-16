32a5cc8735bf370aebdfb9bf719b9656b8922312
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-16 12:28:41 +0530

    Update 1691. Maximum Height by Stacking Cuboids.cpp

--- DP/1691. Maximum Height by Stacking Cuboids.cpp
+++ DP/1691. Maximum Height by Stacking Cuboids.cpp
@@ -7,3 +7,47 @@ Approach :- We first sort the dimennsions of array in increasing order as we can
             So we use Dp to find LIS in O(n^2)
 Time Complexity :- O(n^2)
 */
+
+bool comp(vector<int>&P,vector<int>&Q){
+    if(P[2] == Q[2]){
+        if(P[1] == Q[1])return P[0]>Q[0];
+        return P[1]>Q[1];
+    }
+    return P[2]>Q[2];
+}
+
+class Solution {
+public:
+    
+    int maxHeight(vector<vector<int>>& cuboids) {
+        
+        for(auto&x:cuboids)sort(x.begin(),x.end());
+        
+        sort(cuboids.begin(),cuboids.end(),comp);
+        for(auto&x:cuboids)reverse(x.begin(),x.end());
+        reverse(cuboids.begin(),cuboids.end());
+        //now can I use dp ?
+        //Yes I can 
+         
+        int n = cuboids.size();
+       
+        int mx = 0;
+        vector<int>dp(n,0);
+        for(int i=0;i<n;++i){
+            dp[i] = cuboids[i][0];
+            mx = max(mx,dp[i]);
+            
+             //finding LIS 
+            for(int j=i-1;j>=0;--j){
+                //check if this box can come inside ith vos or not
+                if((cuboids[i][0]<cuboids[j][0]) || (cuboids[i][1]<cuboids[j][1]) || (cuboids[i][2]<cuboids[j][2]))continue;
+                
+                if(dp[i]<(dp[j]+cuboids[i][0])){
+                    dp[i] = dp[j]+cuboids[i][0];
+                    mx = max(mx,dp[i]);
+                }
+            }
+        }
+        return mx;
+    }
+};
