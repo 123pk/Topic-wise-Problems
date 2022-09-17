bfce9a83d339f3c29b6218a3200a7a3f517de9e0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-17 23:05:56 +0530

    Create 2411. Smallest Subarrays With Maximum Bitwise OR.cpp

---
+++ Bit mask/2411. Smallest Subarrays With Maximum Bitwise OR.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Bi Weekly contest 87
+Approach :- So traverse from last to front and maintain one array of bits and one array whcih store the smallest index that set that bit 
+            Now for eachindex the length of smallest subarray is max_value of smallest indexes for each bit - i.
+*/
