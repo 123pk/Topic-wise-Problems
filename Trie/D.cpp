442289f204fc57914e76bb98890307f6153f1aac
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-18 09:38:29 +0530

    Update D.cpp

--- Trie/D.cpp
+++ Trie/D.cpp
@@ -1,4 +1,6 @@
 /*
+Contest :- Leetcode Weekly contest 311
 Approach :- We will maintain a trie which will contain one count array which will be updated every time we visit that character node of trie ,  it is basically frequency arry
             for that character node , now while searching we will find the sum of frequency of all characters in path .
+Time Complexity :- O(n*m) where m is length of longest string
 */
