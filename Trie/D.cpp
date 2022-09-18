2756704414edda83c52f3757eddae65dd433f35c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-18 09:03:08 +0530

    Create D.cpp

---
+++ Trie/D.cpp
@@ -0,0 +1,4 @@
+/*
+Approach :- We will maintain a trie which will contain one count array which will be updated every time we visit that character node of trie ,  it is basically frequency arry
+            for that character node , now while searching we will find the sum of frequency of all characters in path .
+*/
