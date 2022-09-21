b211bb38cca5be1f18f3aa687d8c33456f76a6da
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-21 22:00:13 +0530

    Create 985. Sum of Even Numbers After Queries.cpp

---
+++ Greedy/985. Sum of Even Numbers After Queries.cpp
@@ -0,0 +1,12 @@
+/*
+Platform :- Leetcode
+Approach :- We have four cases :- 
+           1. nums[i] is odd
+               i. val is odd
+               ii. val is even
+           2. nums[i] is even
+               i. val is odd
+               ii. val is even
+           
+Time Complexity :- O(n)
+*/
