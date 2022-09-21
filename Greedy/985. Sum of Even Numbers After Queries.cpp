8984676e7cefe6bd6fd2b1d2b598f91056177269
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-21 22:00:32 +0530

    Update 985. Sum of Even Numbers After Queries.cpp

--- Greedy/985. Sum of Even Numbers After Queries.cpp
+++ Greedy/985. Sum of Even Numbers After Queries.cpp
@@ -10,3 +10,39 @@ Approach :- We have four cases :-
            
 Time Complexity :- O(n)
 */
+class Solution {
+public:
+    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<int>ans;
+        
+        int eve = 0;
+        for(auto&x:nums){
+            if(x%2 == 0)eve += x;
+        }
+        
+        for(auto&x:queries){
+            if(nums[x[1]]%2){
+                if(x[0]%2){
+                    eve += (nums[x[1]] + x[0]);
+                    nums[x[1]]+=x[0];
+                }
+                else{
+                    
+                    nums[x[1]] += x[0];
+                }
+            }
+            else{
+                if(x[0]%2 == 0){
+                    eve += x[0];
+                    nums[x[1]] += x[0];
+                }
+                else{
+                    eve -= nums[x[1]];
+                    nums[x[1]] += x[0];
+                }
+            }
+            ans.push_back(eve);
+        }
+        return ans;
+    }
+};
