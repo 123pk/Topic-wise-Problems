e5a11ab5bc954f96e9fe2ea4ef6a785258868503
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-22 21:18:44 +0530

    Update 1218. Longest Arithmetic Subsequence of Given Difference.cpp

--- DP/1218. Longest Arithmetic Subsequence of Given Difference.cpp
+++ DP/1218. Longest Arithmetic Subsequence of Given Difference.cpp
@@ -5,3 +5,23 @@ Approach :- Sice we have to deal with negative values too , We will add some big
 Time Complexity :- O(n)
 Space Complexity :- O(n)
 */
+class Solution {
+public:
+    int longestSubsequence(vector<int>& arr, int dif) {
+        vector<int>dp(100001);
+                
+        //now we don't need to deal with negative value
+        for(auto&x:arr)x+=50000;
+        
+        int ans = 1;
+        
+        for(auto&x:arr){
+            if((x-dif) <0)continue;
+            // max no of len of AP including 'x' is max of previous len including 'x' and len to reach 'x'-dif + 1  
+            dp[x] = max(1,max(dp[x],dp[x-dif]+1));
+            ans = max(ans,dp[x]);
+        }
+        return ans;
+        
+    }
+};
