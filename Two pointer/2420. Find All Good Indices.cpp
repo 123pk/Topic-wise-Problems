8764b49bcedbb9f545efa37af27e154151d2aa75
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-25 22:45:14 +0530

    Create 2420. Find All Good Indices.cpp

---
+++ Two pointer/2420. Find All Good Indices.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 312
+Approach :- We will try to find length of of longest non-increasing subarray till 'ith' index and similarly length of longest non-decreasing subarray .
+            We will use prefix and suffix array to do so .
+            Now for each index we will check the condition and if it satisfies then it is peak else not .
+Time Complexity :- O(N)
+*/
