73b00282c4c1cbca23517652358fc19ba1ed782d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-25 22:53:41 +0530

    Create 622. Design Circular Queue.cpp

---
+++ Data Structure/622. Design Circular Queue.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- We will use vector and two pointer one will act as start and one as end .
+            Condition for empty queue is when start == -1 or start>end
+*/
