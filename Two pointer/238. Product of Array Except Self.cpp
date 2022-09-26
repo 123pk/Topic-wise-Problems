3299ebe6aae474dd47f81f0c10344b9d0061a419
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-26 23:31:51 +0530

    Create 238. Product of Array Except Self.cpp

---
+++ Two pointer/238. Product of Array Except Self.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Approach :- We need to find prefix and suffix that will contain product of values .
+            Now for index 'i' our required value is pref[i-1]*suff[i+1] for valid idxes.
+Time Complexity:- O(n*n)
+*/
