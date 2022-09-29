2ed62f09e42aafb8c2c9432489b88b9c65ca34c9
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-29 22:06:06 +0530

    Update 658. Find K Closest Elements.cpp

--- Greedy/658. Find K Closest Elements.cpp
+++ Greedy/658. Find K Closest Elements.cpp
@@ -3,3 +3,19 @@ Platform :- Leetcode
 Approach :- Find the abs diff and store it along with value and now sort them in increasing order and get the kth value
 Time Complexity :- O(n Logn)
 */
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int y) {
+        vector<int>ans;
+        vector<pair<int,int>>P;
+        for(auto &x:arr){
+            P.push_back({abs(x-y),x});
+        }
+        
+        sort(P.begin(),P.end());
+        
+        for(int i=0;i<k;++i)ans.push_back(P[i].second);
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
