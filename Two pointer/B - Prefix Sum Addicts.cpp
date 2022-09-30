b7fd1ee94288b8b6e853f63f2f281135ab66863e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-09-30 22:52:29 +0530

    Update B - Prefix Sum Addicts.cpp

--- Two pointer/B - Prefix Sum Addicts.cpp
+++ Two pointer/B - Prefix Sum Addicts.cpp
@@ -6,3 +6,61 @@ Approach :- We will keep on finding the values , A[i] = B[i]-B[i-1] , and all th
               x  = ceil(B[0]/(n-k)+1) .. if this 'x' is greatear than smallest A[i] value that we have calculated then also it is impossible .
  Time Comlexity :- O(k) 
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        
+        long long A[k];
+        for(int i=0;i<k;++i){
+            cin>>A[i];
+        }
+        if(k == 1){
+            cout<<"YES\n";
+            continue;
+        }
+        //point to note we have non decresing array
+        vector<long long>P(n);
+        int j = n-1;
+        int f = 0;
+        long long last;
+        long long mn = 1e18;
+        for(int i=k-2;i>=0;--i){
+            last = A[i+1]-A[i];
+            if(last<0){
+                f=1;
+                break;
+            }
+            if(i<(k-2)){
+                if(last>P[j+1]){
+                    f=1;
+                    break;
+                }
+            }
+           // cout<<last<<" ";
+            P[j] = last;
+            mn = min(mn,last);
+            j--;
+        }
+        //long long mx = P[n-1];
+        
+        //now I want to achieve that last from that
+        //from remaining elements 
+        int rem = (n-k)+1;
+        long long val = A[0];
+        
+        long long high = val/rem;
+        if(val%rem)high++;
+        
+        if(high>mn)f=1;
+        
+        if(f)cout<<"NO\n";
+        else cout<<"YES\n";
+    }
+    
+}
