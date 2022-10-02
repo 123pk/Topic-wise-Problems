af8e34e6fa3e8839427225b0198a69ab75608add
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-02 16:45:44 +0530

    Update 1155. Number of Dice Rolls With Target Sum.cpp

--- DP/1155. Number of Dice Rolls With Target Sum.cpp
+++ DP/1155. Number of Dice Rolls With Target Sum.cpp
@@ -5,3 +5,37 @@ Approach :- We will use recurison  + memoisation ,
             we will take care of overflow using 'mod'
 Time Comlexity :- O(n*k*target)
 */
+
+
+class Solution {
+public:
+    int mod = 1e9+7;
+    int dp[31][1001];
+    
+    int find(int i,int k,int rem){
+        
+        if(i == 0){
+            if(rem)return 0;
+            else return 1;
+        }
+        
+        if(dp[i][rem] != -1)return dp[i][rem];
+        
+        int val = 0;
+        
+        for(int j=1;j<=k;++j){
+            if((rem-j)<0)break;
+            val += find(i-1,k,rem-j);
+            val %= mod;
+        }
+        
+        
+        return dp[i][rem] = val;
+    }
+    int numRollsToTarget(int n, int k, int target) {
+        memset(dp,-1,sizeof(dp));
+        
+        return find(n,k,target);
+        
+    }
+};
