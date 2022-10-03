8fd1e1ed231723038a5479b4c858c165c90ee6f1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-03 22:51:06 +0530

    Update 1578. Minimum Time to Make Rope Colorful.cpp

--- Greedy/1578. Minimum Time to Make Rope Colorful.cpp
+++ Greedy/1578. Minimum Time to Make Rope Colorful.cpp
@@ -7,3 +7,44 @@ Approach :- So the intution is that you need to understand if there are 'x' numb
             the sum of time in range and remove the max time from the sum of range and that remaining time is added to our answer.
 Time Complextiy :- O(n)
 */
+class Solution {
+public:
+    int minCost(string colors, vector<int>& need) {
+        int ans = 0;
+        
+        int n = need.size();
+        
+        vector<pair<int,int>>change;
+        //index and no of consecutive values that are equal
+        int c = 1;
+        
+        for(int i=1;i<n;++i){
+           if(colors[i] == colors[i-1]){
+               c++;
+               if(i == n-1)change.push_back({i,c});
+           }     
+            else{
+                if(c>1){
+                    change.push_back({i-1,c});
+                }
+                c = 1;
+            }
+        }
+        
+        
+        for(auto&x:change){
+            int tot1 = 0;
+            int idx = x.first;
+            int mn = 0;
+            //find sum in range and remove the max value in range from it
+            for(int i=0;i<x.second;++i){
+                tot1 += need[idx-i];
+                mn = max(mn,need[idx-i]);
+            }
+            
+            ans += (tot1 - mn);
+        }
+        
+        return ans;
+    }
+};
