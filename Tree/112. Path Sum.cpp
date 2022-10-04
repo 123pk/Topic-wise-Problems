b32caca05101273041c3908978624e2a3d9850fc
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-04 21:57:59 +0530

    Create 112. Path Sum.cpp

---
+++ Tree/112. Path Sum.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Approach :- BFS and store sum of nodes from root to current node, if we are at leaf and sum is equal to target we have the path .
+
+Time Complexity :- O(n)
+*/
