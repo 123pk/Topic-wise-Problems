ccd0b3ae8db0341469a2a58e2041ff922d253ed2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-04 21:58:21 +0530

    Update 112. Path Sum.cpp

--- Tree/112. Path Sum.cpp
+++ Tree/112. Path Sum.cpp
@@ -4,3 +4,32 @@ Approach :- BFS and store sum of nodes from root to current node, if we are at l
 
 Time Complexity :- O(n)
 */
+class Solution {
+public:
+    
+    bool hasPathSum(TreeNode* root, int targetSum) 
+    {
+       if(!root)return false;
+        
+        queue<pair<TreeNode*,int>>bfs;
+        bfs.push({root,root->val});
+        
+        while(!bfs.empty()){
+            TreeNode* node = bfs.front().first;
+            int val = bfs.front().second;
+            bfs.pop();
+            if(!node->left && !node->right){
+                if(val == targetSum)return true;
+            }
+            
+            if(node->left)
+                bfs.push({node->left,val + node->left->val});
+            
+            if(node->right)
+                bfs.push({node->right,val + node->right->val});
+        }
+        return false;
+    }
+    
+     
+};
