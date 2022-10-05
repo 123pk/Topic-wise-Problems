c5134277ecd71381041d3a6cc39f3eeca444eef4
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-05 13:44:46 +0530

    Create D - Flip and Adjust.cpp

---
+++ DP/D - Flip and Adjust.cpp
@@ -0,0 +1,9 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner Contest 271
+Approach :- We will first come up with recursiv solution where we will use both the option and later we will use memoisation to avoid TLE.
+             So we have two opton 
+             i. Choose value at top
+             ii. Flip the top and get the bottom value
+Time Complexity :- O(n*k)
+*/
