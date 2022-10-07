0dc80caa41c13114bda4a13a12a11cb9c9b752cd
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-07 23:04:53 +0530

    Update 410. Split Array Largest Sum.cpp

--- Binary Search/410. Split Array Largest Sum.cpp
+++ Binary Search/410. Split Array Largest Sum.cpp
@@ -3,3 +3,22 @@ Platform :- Leetcode
 Hint :- Binary seach on the max subarray sum.
 Time Comlexity :- O(n log n)
 */
+int splitArray(vector<int>& nums, int m) {
+        int l=0,r=0,n=nums.size();
+        for(int i=0;i<n;++i) l=max(l,nums[i]), r+=nums[i];
+        
+        int mid=0,ans=0;
+        while(l<=r){
+            mid=(l+r)/2;
+            int count=0,tempsum=0;
+            for(int i=0;i<n;++i){
+                if(tempsum+nums[i]<=mid) tempsum+=nums[i];
+                else count++,tempsum=nums[i];
+            }
+            count++; 
+            
+            if(count<=m) r=mid-1, ans=mid;
+            else l=mid+1;
+        }  
+        return ans;
+    }
