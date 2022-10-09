78517d31369b2db7796d606f9084d9e87405f5b6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-09 21:46:25 +0530

    Create 2435. Paths in Matrix Whose Sum Is Divisible by K.cpp

---
+++ DP/2435. Paths in Matrix Whose Sum Is Divisible by K.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 314
+Appraoch :- We will use dp here , dp[i][j][k] ,this will store number of ways to get 'remainder' of sum equal to 'k' at cell (i,j) . 
+            -- since there cannot be value of k>=50 , we can use this
+Time and Space complexity :- O(n*m*k) .
+*/
