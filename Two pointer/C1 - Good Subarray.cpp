69b00f45c5368e51671ca1ac377c77733a9099d8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-10 22:09:52 +0530

    Update C1 - Good Subarray.cpp

--- Two pointer/C1 - Good Subarray.cpp
+++ Two pointer/C1 - Good Subarray.cpp
@@ -6,3 +6,34 @@ Approach :- So for a given subarray if current element can be added if A[i] >= c
             
 Time Complextiy :- O(n)
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        int A[n];
+        for(int i=0;i<n;++i)cin>>A[i];
+        
+        long long ans = 0,c = 1;
+        for(int i=0;i<n;++i){
+            if(A[i] >= c){
+                ans += c;
+                c++;
+            }
+            else{
+                
+                 while(c>A[i])c--;
+                 ans += c;
+                 c++;
+            }
+           // cout<<ans<<' ';
+        }
+        cout<<ans<<"\n";
+    }
+}
