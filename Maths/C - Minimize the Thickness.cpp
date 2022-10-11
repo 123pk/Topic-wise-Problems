59486ae0949bb4538b913dcc3cd469a8eedf0050
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-11 22:27:38 +0530

    Create C - Minimize the Thickness.cpp

---
+++ Maths/C - Minimize the Thickness.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 826 Div 3
+Approach :- We will find the sum and then store the unique factors in a vector,
+            now for each vector we will find the size of smallest value and that will be our ansewr.
+Time Complexity :- O(n*(sqrt(sum))
+*/
