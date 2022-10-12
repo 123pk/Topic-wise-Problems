cbc71d08e18873f937d46473c2229ce6a45ca05a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-12 22:18:03 +0530

    Create Find Optimally.cpp

---
+++ Prefix/Find Optimally.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Geeksforgeeks
+Contest :- Codefiesta by IIIT Nagpur
+Approach :- We store the frequency array of each string , now we traverse through the query and then look for in frequency array ,the 
+            smallest index where each of character of query are available that is our answer,if we couldnot find any we add -1 as there is no solution.
+*/
