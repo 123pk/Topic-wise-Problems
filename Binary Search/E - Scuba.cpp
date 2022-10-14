73aa4b3cbc3742707734a5b86065b9b6dc4a899c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-14 21:33:36 +0530

    Update E - Scuba.cpp

--- Binary Search/E - Scuba.cpp
+++ Binary Search/E - Scuba.cpp
@@ -6,3 +6,60 @@ Approach :- We will store the sum of height that we can achieve for each of the 
             our soution.
 Time Complexity :- O(n*log(n))
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+         int n,q;
+         cin>>n>>q;
+         
+         long long A[n];
+         map<long long,long long>values;
+         set<long long>steps;
+         long long last,sum = 0;
+         for(int i=0;i<n;++i){
+             cin>>A[i];
+             sum += A[i];
+             if(i == 0){
+                 steps.insert(A[i]);
+                 values[A[i]] = sum;
+                 last = A[i];
+             }
+             else{
+                 if(last >= A[i]){
+                     values[last] = sum;
+                 }
+                 else{
+                     last = A[i];
+                     steps.insert(A[i]);
+                     values[last] = sum;
+                 }
+             }
+         }
+         
+         
+         for(int i=0;i<q;++i){
+             long long x;
+             cin>>x;
+             
+             auto it = steps.lower_bound(x);
+             if(it == steps.end())it--;
+             long long val = *it;
+             long long score = values[val];
+             
+             if(val > x){
+                 if(it == steps.begin())score = 0;
+                 else {
+                     it--;
+                     val = *it;
+                     score = values[val];
+                 }
+             }
+             cout<<score<<" ";
+         }
+         cout<<"\n";
+    }
+}
