7d537929578f56607281b470a37a387b4f9fa878
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-16 22:29:27 +0530

    Update D - Divisibility by 2^n.cpp

--- Maths/D - Divisibility by 2^n.cpp
+++ Maths/D - Divisibility by 2^n.cpp
@@ -7,3 +7,51 @@ Approach :- We will find 2^x that we have in our array from the elements by divi
             
 Time Complexity :- Nlog(N)
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        long long A[n];
+        int c = n;
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            while(A[i]%2ll == 0){
+                A[i]>>=1ll;
+                c--;
+            }    
+        }
+     //  cout<<c<<" ";
+        if(n%2)n--;
+        int op = 0;
+        vector<int>P;
+        for(int i=n;i>=1;i-=2){
+            int d = i;
+            int count = 0;
+            while(d%2 == 0){
+                d/=2;
+                count++;
+            }
+            P.push_back(count);
+        }
+        
+        
+        sort(P.begin(),P.end(),greater<int>());
+        for(int i=0;i<P.size();++i){
+            if(c<=0)break;
+            c -= P[i];
+            op++;
+        }
+        
+        if(c<=0)cout<<op<<"\n";
+        else cout<<"-1\n";
+        
+        
+    }
+}
