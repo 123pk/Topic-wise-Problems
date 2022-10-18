3a9d1471865ec119c88a8be4022243a47a3ebf4c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-18 14:35:59 +0530

    Create C - Even Number Addicts.cpp

---
+++ DP/C - Even Number Addicts.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Global Round 22
+Approach :- We will use (0/1) dp (recursion + memoistaion) where we have two optioon for each player either to choose odd or even term if available .
+             We will choose the options according to moves , bob will want alice to loose and alice will want to win.
+*/
