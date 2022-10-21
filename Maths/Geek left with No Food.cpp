31b5c9eeced904fff6414e2d04855d1d1dcb426d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-21 23:04:04 +0530

    Update Geek left with No Food.cpp

--- Maths/Geek left with No Food.cpp
+++ Maths/Geek left with No Food.cpp
@@ -1,3 +1,50 @@
 /*
 Job-A-thon
 */
+// { Driver Code Starts
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution {
+  public:
+    int solve(int N) {
+        // code here
+        int ans = 0;
+        
+        while(N){
+            if(N%2){
+                int val = (N+1)/2;
+                N -= val;
+            }
+            else N/=2;
+            ans++;
+        }
+        return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        Solution obj;
+        int res = obj.solve(N);
+        
+        cout<<res<<endl;
+        
+    }
+}
+  // } Driver Code Ends
