532b8d38069ec61bf5b68d8034e2b7bdfce0f1c6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-23 21:59:33 +0530

    Create 2448. Minimum Cost to Make Array Equal.py

---
+++ Binary Search/2448. Minimum Cost to Make Array Equal.py
@@ -0,0 +1,7 @@
+'''
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 316
+Approach :- So I did binary search and for each of the value I find the cost , then another cost to one of neighbours of current mid value (mid+1) or (mid-1) I choose the 
+            side where cost was decreasing 
+Time Complexity :- O(n log n)
+'''
