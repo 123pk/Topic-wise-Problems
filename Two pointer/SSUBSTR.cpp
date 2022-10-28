79965f7ad70176b6a49826ef6623a2059bbaa4ed
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-28 22:34:16 +0530

    Update SSUBSTR.cpp

--- Two pointer/SSUBSTR.cpp
+++ Two pointer/SSUBSTR.cpp
@@ -7,3 +7,65 @@ Approach :- We first change out binary string into 10101... or 010101.. or basic
             The min of all is our answer.
 Time Complexity :- O(n)
 */
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n;
+	    cin>>n;
+	    
+	    string s;
+	    cin>>s;
+	    
+	    //let us compress this
+	    string temp = "";
+	    char ch;
+	    for(int i=0;i<n;++i){
+	        if(i == 0){
+	            if(s[i] == '0'){
+	                ch = '0';
+	            }
+	            else ch = '1';
+	            temp += ch;
+	        }
+	        else{
+	            if(s[i] != ch){
+	                ch = s[i];
+	                temp += ch;
+	            }
+	        }
+	    }
+	    
+	    
+	    //now make the count
+	    int c = count(temp.begin(),temp.end(),'0');
+	    int curz = 0,curo = 0;
+	    int ans = INT_MAX;
+	    for(int i=0;i<temp.size();++i){
+	        //we need to get this
+	        if(temp[i] == '1'){
+	            //all values beyond from this point are '1'
+	            int cur = curo + c - curz;
+	            ans = min(ans,cur);
+	            //all ones to left
+	            curo++;
+	        }
+	        else{
+	            
+	            //if this is zero then all points beyond this are '0'
+	            int cur = curo + (c - (curz+1));
+	            ans = min(ans,cur);
+	            curz++;
+	        }
+	        
+	    }
+	    cout<<ans<<"\n";
+	}
+	return 0;
+}
