2d8942c422ffbe3b7244b97dbb1974f33cb14bc8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-10-29 22:19:44 +0530

    Update 2453. Destroy Sequential Targets.cpp

--- Maths/2453. Destroy Sequential Targets.cpp
+++ Maths/2453. Destroy Sequential Targets.cpp
@@ -7,3 +7,36 @@ Approach :- So we are give we can destroy target if nums[j] =  (nums[i] + c*spac
             and one with max frequency/count and min value is our answer.
 Time Complexity:- O(N)
 */
+class Solution {
+public:
+    int destroyTargets(vector<int>& nums, int space) {
+        int ans = 0;
+        sort(nums.begin(),nums.end());
+        
+        map<int,int>count;
+        for(auto&x:nums){
+            int val = x%space;
+            count[val]++;
+        }
+        
+         
+        
+        int mx = 0;
+        int last = 0;
+        for(auto&x:nums){
+             
+            int val = x%space;
+            int req = (space-val);
+            int have = count[val];
+             
+           
+            if(mx<have){
+                mx = have;
+                ans = x;
+            }
+            last = val;
+        }
+        
+        return ans;
+    }
+};
