34a94399a1dd08341223bc93ffb861e3bf97dbfa
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-11-08 20:39:16 +0530

    Create 1544. Make The String Great.cpp

---
+++ Stack/1544. Make The String Great.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Leetcode
+Approach :- We will use a stack and if there is some elemet in stack we will check if top element and current element satisfy the condition or not,if they do then we
+            pop out that element else we keep it and push the current element in stack.
+            After traversign through the string we get the element from stack and add to string and reverse it that is our requried string
+Time Complexity :- O(n)
+*/
