789e31c603046bd0a21bebc3abfdf58540548737
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-11-08 20:39:30 +0530

    Update 1544. Make The String Great.cpp

--- Stack/1544. Make The String Great.cpp
+++ Stack/1544. Make The String Great.cpp
@@ -5,3 +5,29 @@ Approach :- We will use a stack and if there is some elemet in stack we will che
             After traversign through the string we get the element from stack and add to string and reverse it that is our requried string
 Time Complexity :- O(n)
 */
+class Solution {
+public:
+    string makeGood(string s) {
+        string ans = "";
+        stack<char>P;
+        for(int i=0;i<(s.length());++i){
+             if(P.empty()){
+                 P.push(s[i]);
+             }
+             else{
+                 char ch = P.top();
+                 if(abs(ch-s[i]) == 32){
+                     P.pop();
+                 }
+                 else P.push(s[i]);
+             }
+        }
+
+        while(!P.empty()){
+            ans += P.top();
+            P.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
