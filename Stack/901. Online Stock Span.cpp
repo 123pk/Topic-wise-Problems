68ec5d317e98a2ef49c581fdb3fbc0c88e1b760b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-11-09 15:00:51 +0530

    Update 901. Online Stock Span.cpp

--- Stack/901. Online Stock Span.cpp
+++ Stack/901. Online Stock Span.cpp
@@ -5,3 +5,32 @@ Approach :- We will use a stack which will store "value" and "count" , where cou
             at the end we push current value and answer to stack.
 Time Complexity :- O(n)
 */
+class StockSpanner {
+public:
+    // we wnat to know the number of consecutive elemeents that 
+    //are <= current price
+    stack<pair<int,int>>P;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int ans = 1;
+        while(!P.empty()){
+            //get the max value
+            if(P.top().first<=price){
+                ans += P.top().second;
+                P.pop();
+            } 
+            else break;
+        }
+        P.push({price,ans});
+        return ans;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
