6f9eedb6b4a5c6090c472158853a1a738265a65c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-11-10 22:22:56 +0530

    Update Prime Factor Division.cpp

--- Maths/Prime Factor Division.cpp
+++ Maths/Prime Factor Division.cpp
@@ -3,3 +3,42 @@ Platform :- Codechef
 Contest :- Codechef Starters 64B
 Approach :- 
 */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    long long a,b;
+	    cin>>a>>b;
+	    
+	    
+	     long long val = __gcd(a,b);
+	     int f = 0;
+	     b/=val;
+	     while(1){
+	         
+	         long long v2 = b;
+	         long long v3 = __gcd(val,v2);
+	         
+	         if(v3 == 1 && v2!=1){
+	             f=1;
+	             break;
+	         }
+	         else if(v2 == 1 && v3 == 1)break;
+	         
+	         while(v2%v3 == 0){
+	             v2/=v3;
+	         }
+	        // cout<<v2<<" "<<v3<<"\n";
+	         b = v2;
+	     }
+	     
+	    if(f)cout<<"NO\n";
+	    else cout<<"YES\n";
+	}
+	return 0;
+}
