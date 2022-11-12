7e1586657a05b2565f5285a02699174d677f3251
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-11-12 21:16:49 +0530

    Update Magic Well of Lillies.cpp

--- DP/Magic Well of Lillies.cpp
+++ DP/Magic Well of Lillies.cpp
@@ -6,3 +6,47 @@ Approach :- So we will traverse though from 1 to n and for each i, we will find 
             And if its is prime number them dp[i] = min(dp[i],dp[i-1]+1)
 Time Complexity :- O(n* sqrt(n))
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int find(int n,int i){
+    int score = n;
+    int cur = (n/i);
+    int val = (cur+1)*2;
+    int z = cur*i;
+    val += (n - z);
+    score = min(score,val);
+    return score;
+}
+ 
+int main(){
+    int t;
+    cin>>t;
+    
+    for(int z=1;z<=t;++z){
+        cout<<"Case #"<<z<<": ";
+        int n;
+        cin>>n;
+        
+        if(n<=5){
+            cout<<n<<"\n";
+            continue;
+        }
+        vector<int> dp(n+1,n);
+        for(int i=1;i<=n;++i){
+            //all the factors
+            int val = i;
+            for(int j=2;j*j<=i;++j){
+                if(i%j == 0){
+                    int f = i/j;
+                    val = min(val,find(i,f)+score[f]);
+                    val = min(val,find(i,j)+score[j]);
+                }
+            }
+            dp[i] = min(dp[i],val);
+            dp[i] = min(dp[i],dp[i-1]+1);
+        }
+       // cout<<score[48]<<" ";
+        cout<<dp[n]<<"\n";
+    }
+}
