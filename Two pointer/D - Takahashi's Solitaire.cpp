74ef4948098ecfd7da06d99c590685af179faf63
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-11-12 20:59:28 +0530

    Create D - Takahashi's Solitaire.cpp

---
+++ Two pointer/D - Takahashi's Solitaire.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Begineer Contest 277
+Approach :- We will find the max subarray sum which with consecutive or equal values when sorted in incresing order, keep in mind after last element we move to the first in 
+            some case.
+            -- Total left over is total sum - max_subarray_sum
+Time Complexity :- O(n)
+*/
