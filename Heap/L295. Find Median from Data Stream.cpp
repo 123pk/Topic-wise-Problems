81c5840ca471608b7da6f7b832996ae4cadab24d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-11-13 21:17:31 +0530

    Update L295. Find Median from Data Stream.cpp

--- Heap/L295. Find Median from Data Stream.cpp
+++ Heap/L295. Find Median from Data Stream.cpp
@@ -6,3 +6,56 @@ Approach :- We will maintain two heaps , the main idea is to store the first hal
             So we greediy insert the values and at any point the difference in size should not be more than '1'
  Time Complexity :- O(n*logn)
 */
+ 
+class MedianFinder {
+public:
+    //we need two heaps one for left half
+    //one for right half
+    priority_queue<int>small;
+    priority_queue<int,vector<int>,greater<int>>big;
+    int c = 0,d = 0;
+    MedianFinder() {
+        
+    }
+     
+    void addNum(int num) {
+        //this is the tricky part
+        if(c == d){
+            if(d && abs(big.top())<num){
+               small.push(abs(big.top()));
+               big.pop();
+               big.push(num);
+            }
+            else small.push(num);
+             c++;
+        }
+        else if(c>d){
+            if(num<small.top()){
+                big.push(small.top());
+                small.pop();
+                small.push(num);
+                
+            }
+            else big.push(num);
+            d++;
+        }
+        
+    }
+    
+    double findMedian() {
+        double ans = small.top();
+        if((c+d)%2 == 0){
+            ans += big.top();
+            ans/=2.0;
+        }
+        return ans;
+
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
