1aeada377d367a0338da452f35e622ce6a45620a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-11-13 21:16:30 +0530

    Create L295. Find Median from Data Stream.cpp

---
+++ Heap/L295. Find Median from Data Stream.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Leetcode
+Approach :- We will maintain two heaps , the main idea is to store the first half in max heap and second halp of sorted array in min_heap ,  so the top value of both would 
+            represent the middle values of sorted array.
+            - We need to carefully insert the values . We don't want the top value of first half be "greater" than "top" value of min_heap which store the second half.
+            So we greediy insert the values and at any point the difference in size should not be more than '1'
+*/
