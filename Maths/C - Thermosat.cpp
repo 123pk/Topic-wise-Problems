d863c57b56749883c4e0624a76b8adbadee4b741
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-11-18 22:23:44 +0530

    Create C - Thermosat.cpp

---
+++ Maths/C - Thermosat.cpp
@@ -0,0 +1,9 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 834
+Approach:- We don't have any solution when max distance of a and b from fathest end is <= x
+           1. if a and b are same then there is no need of any operatino 
+           2. if abs(a-b)>=x then 1 operation is rquired
+           3. Get to one of the end end try to get to b if it is possible then 2 operation
+           4. else 3 opearation
+*/
