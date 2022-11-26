7668d4b1d8552ed9a6fe33ef7af4658f5e8aaa0f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-11-26 22:11:44 +0530

    Create A -Doremy's Paint.cpp

---
+++ Ad-hoc/A -Doremy's Paint.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Global Round 24
+Approach :- So it is of no use if we use a distinct value in our subarray as it will not effect our answer, so we can add any number of distinct values 
+             so [1,n] is one of the subarrays which give us the max score.
+Time Complexity :-O(n) -- reading input
+*/
