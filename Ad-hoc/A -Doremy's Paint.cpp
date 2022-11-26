08b78b654d892f4b6186afaca851df7f658c6c6f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-11-26 22:12:03 +0530

    Update A -Doremy's Paint.cpp

--- Ad-hoc/A -Doremy's Paint.cpp
+++ Ad-hoc/A -Doremy's Paint.cpp
@@ -5,3 +5,24 @@ Approach :- So it is of no use if we use a distinct value in our subarray as it 
              so [1,n] is one of the subarrays which give us the max score.
 Time Complexity :-O(n) -- reading input
 */
+#include<bits/stdc++.h>
+using namespace std;
+ 
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        vector<int>A(n);
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            
+        }
+        
+        cout<<1<<" "<<n<<"\n";
+    }
+}
