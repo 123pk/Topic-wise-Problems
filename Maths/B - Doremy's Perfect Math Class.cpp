4a029d2edc355a410e38ec17ec458124ee2c3b80
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-11-26 22:14:18 +0530

    Create B - Doremy's Perfect Math Class.cpp

---
+++ Maths/B - Doremy's Perfect Math Class.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Global Round 24
+Approach :- We will find the GCD of array and if it is '1' then we can have all values from 1 to A[n-1],
+            else we can have total number of value = (A[n-1]/gcd)
+Time Complexity :- O(n)
+*/
