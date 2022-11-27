0e5e271692c5be7c3be61f1b30b188f1582a4d55
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-11-27 23:43:03 +0530

    Update 2488. Count Subarrays With Median K.cpp

--- Prefix/2488. Count Subarrays With Median K.cpp
+++ Prefix/2488. Count Subarrays With Median K.cpp
@@ -4,5 +4,7 @@ Contest :- Leetcode Weekly contest 321
 Approach :- It is one of those repeated subarray problems which we have standard approach to solve them .
             -- We get all the value < k and replace with -1
             -- we get all the values >= k and replace with 1
-            Now problem is narrowed down to find number of subarray with sum 1
+            Now problem is narrowed down to find number of subarray with sum 1.
+            Which we solve using map and prefix sum .
+Time Comlexity :- O(n log n)
 */
