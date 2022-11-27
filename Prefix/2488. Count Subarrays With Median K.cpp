ca541b4af0c7104d150c316ac9dbb95a3385a2f4
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-11-27 23:40:53 +0530

    Create 2488. Count Subarrays With Median K.cpp

---
+++ Prefix/2488. Count Subarrays With Median K.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 321
+Approach :- It is one of those repeated subarray problems which we have standard approach to solve them .
+            -- We get all the value < k and replace with -1
+            -- we get all the values >= k and replace with 1
+            Now problem is narrowed down to find number of subarray with sum 1
+*/
