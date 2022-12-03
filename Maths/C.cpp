23a06c66fe76322e857067bab17c96dee0461c0d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-03 22:02:38 +0530

    Update C.cpp

--- Maths/C.cpp
+++ Maths/C.cpp
@@ -1,7 +1,10 @@
 /*
 Platform :- Codechef
 Contest :- Codechef Starters 67
-Approach :- 
+Approach :- If you will carefully observe you will find that it is always optimal to swap the big with the smallest and so on second largest with second smallest..
+            On each swapping we add some amount of inversions which you have to construct the relation.
+            
+Editorial :- https://discuss.codechef.com/t/maxagry-editorial/104323            
 
 Time Complexity :- O(1)
 */
