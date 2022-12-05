3730ac4a46c41b85ca3e1662278f9624c0656481
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-05 21:25:52 +0530

    Update 2492. Minimum Score of a Path Between Two Cities.cpp

--- Graph/2492. Minimum Score of a Path Between Two Cities.cpp
+++ Graph/2492. Minimum Score of a Path Between Two Cities.cpp
@@ -4,3 +4,38 @@ Contest :- Leetcode Weekly contest 322
 Approach :- We traverse through the graph and find the smallest weighted edge and that is out answer.
 Time Complexity :- O(n+m)
 */
+class Solution {
+public:
+    
+    void find(int node,vector<vector<pair<int,int>>>&graph,vector<int>&vis,int& ans){
+        vis[node]++;
+        
+        for(auto&x:graph[node]){
+             
+            int cur_node = x.first;
+            int cur_val = x.second;
+            ans = min(ans,cur_val);
+            if(vis[cur_node])continue;
+            
+            find(cur_node,graph,vis,ans);
+                 
+        }
+         
+    }
+    
+    int minScore(int n, vector<vector<int>>& roads) {
+        //create graph
+        vector<vector<pair<int,int>>>graph(n);
+        for(auto&x:roads){
+            graph[x[0]-1].push_back({x[1]-1,x[2]});
+            graph[x[1]-1].push_back({x[0]-1,x[2]});
+        }
+        vector<int>vis(n);
+      //  vector<int>dist(n,INT_MAX);
+        int ans = INT_MAX;
+        find(0,graph,vis,ans);
+        
+        
+        return ans;
+    }
+};
