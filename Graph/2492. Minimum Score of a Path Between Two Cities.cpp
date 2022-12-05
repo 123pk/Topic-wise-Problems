076cec7e30871de11e521ed66e7e022266f973a1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-05 21:25:34 +0530

    Create 2492. Minimum Score of a Path Between Two Cities.cpp

---
+++ Graph/2492. Minimum Score of a Path Between Two Cities.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 322
+Approach :- We traverse through the graph and find the smallest weighted edge and that is out answer.
+Time Complexity :- O(n+m)
+*/
