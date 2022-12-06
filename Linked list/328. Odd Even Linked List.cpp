4b78f1d3455002c6085837275f9791effb8b8f04
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-06 22:23:42 +0530

    Update 328. Odd Even Linked List.cpp

--- Linked list/328. Odd Even Linked List.cpp
+++ Linked list/328. Odd Even Linked List.cpp
@@ -4,3 +4,36 @@ Approach:- We create two different linked list ,one for odd and one for even val
            of odd linked list
 Time Complexity :- O(n)
 */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+       if(head == NULL || head->next == NULL)
+        return head;
+    ListNode *odd = head;
+    ListNode *even_head = head->next;
+    ListNode *even = even_head;
+    
+    while(even != NULL && even->next != NULL)
+    {
+        odd->next = even->next;
+        odd = odd->next;
+        even->next = odd->next;
+        even = even->next;
+    }
+    odd->next = even_head;
+    return head;
+        
+       
+    }
+    
+};
