4bfdc8b3edd0890d0be6049703c9d3c5239f1956
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-06 22:22:20 +0530

    Create 328. Odd Even Linked List.cpp

---
+++ Linked list/328. Odd Even Linked List.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Approach:- We create two different linked list ,one for odd and one for even values and then at the end we join tail of odd linked list with head of even and reutrn head 
+           of odd linked list
+Time Complexity :- O(n)
+*/
