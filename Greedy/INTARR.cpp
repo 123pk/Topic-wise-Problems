df1ba182d77f135bc26857f500807546cd5f887a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-08 20:58:49 +0530

    Update INTARR.cpp

--- Greedy/INTARR.cpp
+++ Greedy/INTARR.cpp
@@ -12,3 +12,99 @@ Approach :- If you will observe you will find that if frequency of any element >
 
 Time Complexity :- O(n log n) --sorting
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+bool check(vector<long long>&temp){
+    int n = temp.size();
+    for(int i=2;i<n;++i){
+        //increasing
+        if(temp[i-2]<=temp[i-1] && temp[i-1]<=temp[i])return false;
+        
+        //decreasing
+        if(temp[i-2]>=temp[i-1] && temp[i-1]>=temp[i])return false;
+    }
+    return true;
+}
+
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        long long A[n];
+        map<long long,int>count;
+        int lim = (n+1)/2;
+        
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            count[A[i]]++;
+            if(count[A[i]]>lim && lim != -1){
+                lim = -1;
+            }
+        }
+        
+        if(lim == -1){
+            cout<<lim<<"\n";
+            continue;
+        }
+        
+        sort(A,A+n);
+        
+        //modify
+        vector<long long>small,big;
+        int idx = (n+1)/2;
+        for(int i=0;i<idx;++i){
+            small.push_back(A[i]);
+            if((i+idx)<n)big.push_back(A[i+idx]);
+        }
+        
+        //start with small
+        vector<long long>temp1;
+        int j = 0;
+        for(int i=0;i<small.size();++i){
+            temp1.push_back(small[i]);
+            if(j<big.size()){
+                temp1.push_back(big[j]);
+                j++;
+            }
+        }
+        
+        while(j<big.size()){
+            temp1.push_back(big[j]);
+            j++;
+        }
+        
+        
+        if(check(temp1)){
+            for(auto&x:temp1)cout<<x<<" ";
+            cout<<"\n";
+            continue;
+        }
+        
+        //start with big
+        j = 0;
+        temp1.clear();
+        for(int i=0;i<big.size();++i){
+            temp1.push_back(big[i]);
+            if(j<small.size()){
+                temp1.push_back(small[j]);
+                j++;
+            }
+        }
+        while(j<small.size()){
+            temp1.push_back(small[j]);
+            j++;
+        }
+        
+        if(check(temp1)){
+            for(auto&x:temp1)cout<<x<<" ";
+            cout<<"\n";
+        }
+        else cout<<"-1\n";
+    }
+}
