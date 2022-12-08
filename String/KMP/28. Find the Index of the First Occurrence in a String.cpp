75213c0ae2bbbb4ff41f514fb3510e9f2da1f14d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-08 17:49:46 +0530

    Update 28. Find the Index of the First Occurrence in a String.cpp

--- String/KMP/28. Find the Index of the First Occurrence in a String.cpp
+++ String/KMP/28. Find the Index of the First Occurrence in a String.cpp
@@ -3,3 +3,55 @@ Platform :- Leetcode
 Approach :- We will use KMP algorithms for efficiently comparing the strings .
 Time Complexity :- O(n+m)
 */
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        //this problem is good for using kmp
+        
+        int m = needle.size();
+        int n = haystack.size();
+        if(m>n)return -1;
+        
+        vector<int>pre_process(m);
+        int j = 0;
+        for(int i=1;j<m && i<m;){
+            if(needle[i] == needle[j]){
+                j++;
+               
+                pre_process[i] = j;  i++;
+            }
+            else{
+                if(j){
+                   j = pre_process[j-1]; 
+                }
+                else {
+                    pre_process[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        
+        
+        int ans = -1;
+         j = 0;
+        //now we compare
+
+        for(int i=0;i<n;){
+           if(haystack[i] == needle[j]){
+               j++;
+               if(j == needle.size()){
+                   return ((i+1)-j);
+               }++i;
+           }    
+           else{
+              //we need to move a to left
+              if(j == 0){
+                  i++;continue;
+              }
+              else j = pre_process[j-1];
+           }
+        }
+        return ans;
+    }
+};
