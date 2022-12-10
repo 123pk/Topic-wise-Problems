e65c549fc69fa3c7abf97819ae1a4ed99be1b902
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-10 22:47:08 +0530

    Create 2498. Frog Jump II.cpp

---
+++ Greedy/2498. Frog Jump II.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Leetcode
+Contest :- Bi Weekly contest 93
+Approach :- If you will observe you will find that it is Not Optimal to skip two consecutive rocks, so either we can start with '1' or '2' and get all the
+            alternating rocks from there after and find the max value.
+Time Complexity :- O(n)
+*/
