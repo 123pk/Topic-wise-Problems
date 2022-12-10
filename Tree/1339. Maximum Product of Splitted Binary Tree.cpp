c1a50b000be8d7fa325ce20b3904ec2deeb5f746
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-10 22:39:33 +0530

    Create 1339. Maximum Product of Splitted Binary Tree.cpp

---
+++ Tree/1339. Maximum Product of Splitted Binary Tree.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Approach :- We will find total sum of all the nodes,then we will do dfs and try to find sum of nodes in subtree of current node, we will find the product if we seperate t
+            current node from its ansestor and store the max of all.
+Time Complexity :- O(n+e)
+*/
