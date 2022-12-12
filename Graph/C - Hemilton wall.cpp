57a169e8987c6c1163321217cebfb0d7f2b0a54a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-12 22:31:50 +0530

    Create C - Hemilton wall.cpp

---
+++ Graph/C - Hemilton wall.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforce Educational Round 139
+Approach :- We will do dfs , if we start from 0,0 -- we can go 'Down' to 1,0 or Right to '0,1' we will try both option and chek if we can visit all the 'B' cell 
+           or not . Similarly if we start from 1,0 we can go to UP 0,0 or right 1,1 we will try this two option also .
+           We will only move Right ,Down and UP .
+ Time Complexity :- O(m*n)
+*/
