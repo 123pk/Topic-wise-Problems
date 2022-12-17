064533f7e38c8b43fca022be0fe59bd6f574f0f3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-17 22:56:44 +0530

    Update B - Coloring .cpp

--- Maths/B - Coloring .cpp
+++ Maths/B - Coloring .cpp
@@ -6,3 +6,54 @@ Approach :- The main idea is to have a block of size 'k' and we can have 'k' dis
             Else we will sort the values and choose the smallest (m-(k+1)) values to make the set of values from ((m-k) to m) equal to (max_value-1)
 Time Complexity :- O(n log n)
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n,m,k;
+        cin>>n>>m>>k;
+        
+        long long A[m];
+        for(int i=0;i<m;++i)cin>>A[i];
+        
+        sort(A,A+m);
+        //we need to understand this 
+        
+        if(k>m)cout<<"NO\n";
+        else{
+            
+            int f = 0;
+            long long rem = 0;
+            long long sum = 0;
+            for(int i=0;i<(m-k);++i){
+                sum += A[i];
+            }
+            
+            //I want to convert it to A[m-1]-1
+            long long lim = A[m-1]-1;
+            
+            for(int i=m-k;i<m;++i){
+                if(A[i]>=lim)continue;
+                long long req = lim - A[i];
+                if(sum<req){
+                    f=1;
+                    break;
+                }
+                A[i] = lim;
+                sum -= req;
+            }
+            //cout<<"*";
+            if(f)cout<<"NO\n";
+            else {
+                 
+                cout<<"YES\n";
+                
+            }
+        }
+        
+    }
+}
