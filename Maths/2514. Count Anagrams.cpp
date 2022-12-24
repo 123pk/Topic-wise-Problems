5ca9c2680e1d398d2b4ebd095cd9c5f37c628107
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-24 22:32:05 +0530

    Create 2514. Count Anagrams.cpp

---
+++ Maths/2514. Count Anagrams.cpp
@@ -0,0 +1,10 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Bi Weekly contest 94
+Approach :- If you will observe we need to find product of no of distinct permutation of each string .
+            We need to take care of modulo;
+            We will use Little fermant theorem to find no of distict permutation
+More help :- https://stackoverflow.com/questions/32713762/distinct-permutations-of-a-string-modulo-a-prime
+Time Complexity :- O(n*m*log(m!%mod))
+*/
+
