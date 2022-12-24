20666ff5cdee97d8febd5100f2d5f81b52d5d19d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-24 22:38:53 +0530

    Update D - Scope.cpp

--- Stack/D - Scope.cpp
+++ Stack/D - Scope.cpp
@@ -8,3 +8,61 @@ Approach:- We maintain one frequency array of size 26 where 0-->a ,1 -->b ... 25
            else we can always make the string empty
 Time Complexity :- O(n)
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+      string s;
+      cin>>s;
+       
+      
+      int c = 0;
+      stack<char>P;
+      int n = s.size();
+      vector<vector<int>>Q(n,vector<int>(26));
+      vector<int>fre(26),count(26);
+      
+      int f = 0;
+      
+      for(int i=0;i<s.size();++i){
+          if(s[i] == '('){
+              P.push(s[i]);
+          }
+          else if(s[i] == ')'){
+               int g = 0;
+               while(!P.empty()){
+                   if(P.top() == '('){
+                       P.pop();
+                       g=1;
+                       break;
+                   }
+                   else {
+                       int idx = P.top()-'a';
+                       count[idx]--;
+                       P.pop();
+                   }
+               }
+               if(g == 0){
+                   f=1;
+                   break;
+               }
+          }
+          else{
+              int cur = s[i]-'a';
+              if(count[cur]){
+                  f=1;
+                  break;
+              }
+              count[cur]++;
+              P.push(s[i]);
+          }
+         
+      }
+      
+      
+      if(f == 0){
+          
+         cout<<"Yes";
+      }
+      else cout<<"No";
+}
