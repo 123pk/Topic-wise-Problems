ed73b0530932d08064b22ff9bd8ccc17e384edf2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-26 19:54:59 +0530

    Update 2517.Maximu Tastiness of candy basket.cpp

--- Binary Search/2517.Maximu Tastiness of candy basket.cpp
+++ Binary Search/2517.Maximu Tastiness of candy basket.cpp
@@ -8,3 +8,40 @@ Approach:- We will binary serach on values between 1 and max value,we will also 
            
 Time Complexity :- O(N log N)
 */
+class Solution {
+public:
+    int maximumTastiness(vector<int>& price, int k) {
+        //binary search
+        int ans = 0;
+        sort(price.begin(),price.end());
+        int n = price.size();
+        int start = 1,end = (price[n-1]),mid =(start+(end-start)/2);
+        
+        while(start<=end){
+            
+            //we check if we can have 
+            int last = price[0];
+            int c = 1;
+            for(int i=1;i<n;++i){
+                if(price[i]-last >= mid){
+                    c++;
+                    last = price[i];
+                }
+            }
+            //cout<<c<<" "<<mid<<" "<<start<<" "<<end<<"\n";
+            if(c>=k){
+                ans = max(ans,mid);
+                start = mid+1;
+                mid = (start+(end-start)/2);
+            }
+            else{
+                //move down the limit
+                end = mid-1;
+                mid = (start+(end-start)/2);
+            }
+        }
+        
+        
+        return ans;
+    }
+};
