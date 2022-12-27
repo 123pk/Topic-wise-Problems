72c90665f1935035fd258660091eabf810d9363c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-27 22:43:56 +0530

    Update 2279. Maximum Bags With Full Capacity of Rocks.cpp

--- Greedy/2279. Maximum Bags With Full Capacity of Rocks.cpp
+++ Greedy/2279. Maximum Bags With Full Capacity of Rocks.cpp
@@ -3,3 +3,27 @@ Platform :- Leetcode
 Approach :- Greedy
 Time Complexity :- O(n log n)
 */
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int add) {
+        
+        int ans = 0;
+        int i = 0;
+        for(auto&x:capacity){
+            x = x - rocks[i];
+            i++;
+        }
+        
+        sort(capacity.begin(),capacity.end());
+        for(int i = 0;i<capacity.size() && add>0;++i){
+            if(capacity[i]<=add){
+                ans++;
+                add-=capacity[i];
+            }
+            else break;
+        }
+        
+        
+        return ans;
+    }
+};
