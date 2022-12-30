eca7444e503e623ce668725d8f43f84ac3258f22
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-30 22:56:46 +0530

    Create A - Koxia and whiteboard.cpp

---
+++ Brute force/A - Koxia and whiteboard.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Bye 2022
+Approach:- We will perform the operation m times and each time we will update the smallest value as it will have the max difference that we can have .
+           at the end find the sum
+Time Complexity :- O(n log n*m)
+*/
