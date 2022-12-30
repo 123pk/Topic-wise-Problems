76a2713341153a968eb626e6336f6368748e4f51
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-12-30 22:57:28 +0530

    Update A - Koxia and whiteboard.cpp

--- Brute force/A - Koxia and whiteboard.cpp
+++ Brute force/A - Koxia and whiteboard.cpp
@@ -5,3 +5,39 @@ Approach:- We will perform the operation m times and each time we will update th
            at the end find the sum
 Time Complexity :- O(n log n*m)
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,m;
+        cin>>n>>m;
+        
+        long long A[n];
+        long long tot = 0;
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            tot += A[i];
+        }
+        
+        long long B[m];
+        for(int i=0;i<m;++i){
+            cin>>B[i];
+        }
+        
+        sort(A,A+n);
+        int j = 0;
+        while(j<m){
+            sort(A,A+n);
+            
+            tot += B[j]-A[0];
+            A[0] = B[j];
+            j++;
+        }
+        
+        cout<<tot<<"\n";
+    }
+}
