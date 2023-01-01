1d36468523c18e3885f1e90378b0e8d885f61516
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-01 21:29:46 +0530

    Rename Maths/Sieve/Closest Prime Numbers in range.cpp to Maths/Closest Prime Numbers in range.cpp

---
+++ Maths/Closest Prime Numbers in range.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 326
+Approach :- We will find the prime factors till 'right' using seive,now we will traverse through the array storing the prime and find the prime with smallest difference 
+           between them and add to the answer.
+Time Comlexity :- O(n)
+Space Complexity :- O(n)
+*/
