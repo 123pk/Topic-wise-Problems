59484858612ee42cd4316891f7cbe164e8f4f1ab
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-04 22:40:15 +0530

    Create Maximu Points covered.cpp

---
+++ DP/Maximu Points covered.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Geeksforgeeks
+Contest :- Mega Job-A-thon 4 january
+Approach :- First create on 2d dp where dp[i][j] -- store the no of points in rectangle from 1,1 to i,j
+            we create 2d dp of size (max_x coordinate,max_y coordinate) in array.
+Time Compelxity :- O(n^2)
+Space Complexity :- O(mxx*mxy) - O(n^2)
+*/
