268b99755c6aadb03d8475173cb77fe307aca7d6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-05 21:52:14 +0530

    Update 29. Longest Increasing Path in a Matrix.cpp

--- DP/29. Longest Increasing Path in a Matrix.cpp
+++ DP/29. Longest Increasing Path in a Matrix.cpp
@@ -6,3 +6,72 @@ Approach :- So the basic idea is to dfs and we find the max length path we can g
             we check all the four option if it is valid to move and we have not already visited and find its value we go there and finnd the value ,mark it visited.
 Time Complexity :- 
 */
+class Solution {
+public:
+    
+    int find(int i,int j,vector<vector<int>>&matrix,vector<vector<int>>&score,vector<vector<int>>&vis){
+        vis[i][j]++;
+      //  cout<<i<<" "<<j<<endl;
+        int val = 0;
+        //we can move in four directions
+        if((i-1)>=0 && matrix[i-1][j]<matrix[i][j]){
+            if(score[i-1][j]){
+                val = max(val,score[i-1][j]);
+            }
+            else if(vis[i-1][j] == 0){
+               val = max(val,find(i-1,j,matrix,score,vis));    
+            }
+        }
+        
+        if((i+1)<matrix.size() && matrix[i+1][j]<matrix[i][j]){
+            if(score[i+1][j]){
+                val = max(val,score[i+1][j]);
+            }
+            else if(vis[i+1][j] == 0){
+               val = max(val,find(i+1,j,matrix,score,vis));    
+            }
+        }
+        
+        if((j-1)>=0 && matrix[i][j-1]<matrix[i][j]){
+            if(score[i][j-1]){
+                val = max(val,score[i][j-1]);
+            }
+            else if(vis[i][j-1] == 0){
+               val = max(val,find(i,j-1,matrix,score,vis));    
+            }
+        }
+        
+        if((j+1)<matrix[0].size() && matrix[i][j+1]<matrix[i][j]){
+            if(score[i][j+1]){
+                val = max(val,score[i][j+1]);
+            }
+            else if(vis[i][j+1] == 0){
+               val = max(val,find(i,j+1,matrix,score,vis));    
+            }
+        }
+            
+            
+        return score[i][j] = 1+val;
+    }
+    
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int ans = 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        vector<vector<int>>score(n,vector<int>(m));
+        vector<vector<int>>vis = score;
+      //  cout<<n<<" "<<m<<endl;
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(vis[i][j])continue;
+                int val = find(i,j,matrix,score,vis);
+                ans = max(ans,val);
+               // cout<<val<<" ";
+            }
+             
+        }
+        
+        return ans;
+    }
+};
