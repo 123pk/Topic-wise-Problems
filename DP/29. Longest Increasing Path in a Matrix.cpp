85279dfe722503fdbf9f542d99adfb1f5c00fc88
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-05 21:52:41 +0530

    Update 29. Longest Increasing Path in a Matrix.cpp

--- DP/29. Longest Increasing Path in a Matrix.cpp
+++ DP/29. Longest Increasing Path in a Matrix.cpp
@@ -4,7 +4,7 @@ Approach :- So the basic idea is to dfs and we find the max length path we can g
             if required so that we don't compute for same cell again and again (Dynamic Programming).
             We have four option to move from any cell (left,right ,up and down)
             we check all the four option if it is valid to move and we have not already visited and find its value we go there and finnd the value ,mark it visited.
-Time Complexity :- 
+Time Complexity :- O(m*n)
 */
 class Solution {
 public:
