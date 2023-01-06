61a008627c99357d375c8650d1efe4c668a73922
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-06 23:14:42 +0530

    Update Most stones removed with same row or column .cpp

--- Graph/Most stones removed with same row or column .cpp
+++ Graph/Most stones removed with same row or column .cpp
@@ -7,3 +7,52 @@ Approach :- If you will carfully observe you will find that there is formation o
             and keep on counting.
  Time Complexiy :- O(m*n*log(m*n))
 */
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        //dsu
+        map<int,vector<int>>row,col;
+        set<pair<int,int>>used;
+        int i = 0,ans= 0;
+        for(auto&x:stones){
+            row[x[0]].push_back(i);
+            col[x[1]].push_back(i);
+            i++;
+        }
+        
+        for(auto&x:stones){
+            if(used.size()){
+                if(used.find({x[0],x[1]}) != used.end())continue;
+            }
+            used.insert({x[0],x[1]});
+            int count = 1;
+            queue<pair<int,int>>bfs;
+            bfs.push({x[0],x[1]});
+            
+            while(!bfs.empty()){
+                pair<int,int>temp = bfs.front();
+                bfs.pop();
+                int r = temp.first;
+                int c = temp.second;
+                
+                for(auto&x:row[r]){
+                    if(used.find({stones[x][0],stones[x][1]}) != used.end())continue;
+                    count++;
+                    used.insert({stones[x][0],stones[x][1]});
+                    bfs.push({stones[x][0],stones[x][1]});
+                }
+                
+                for(auto&x:col[c]){
+                    if(used.find({stones[x][0],stones[x][1]}) != used.end())continue;
+                    count++;
+                    used.insert({stones[x][0],stones[x][1]});
+                    bfs.push({stones[x][0],stones[x][1]});
+                }
+            }
+            
+            
+            ans += count-1;
+        }
+        return ans;
+    }
+};
