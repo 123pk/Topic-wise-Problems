7a59068db3694e9b17e193ecb1214d33a39dd6d9
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-08 22:54:06 +0530

    Create A - Make it beautiful.cpp

---
+++ Maths/A - Make it beautiful.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Educational round 141
+Appraoch :- Sort the values in increasing order ,now reverse them and swap second and last element if possible .
+           In this way we are not going to have any array whose sum of prefix [i-1] = A[i] until all values are same
+Time Complexity :- O(n log n)
+*/
