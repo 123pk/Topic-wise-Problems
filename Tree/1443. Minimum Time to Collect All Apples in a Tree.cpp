35ebcc09167ea2601a7d77754bb1007803e0d4db
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-11 23:35:35 +0530

    Update 1443. Minimum Time to Collect All Apples in a Tree.cpp

--- Tree/1443. Minimum Time to Collect All Apples in a Tree.cpp
+++ Tree/1443. Minimum Time to Collect All Apples in a Tree.cpp
@@ -4,3 +4,36 @@ Approach :- We will do dfs and if there is any apple in descendents of current n
             So we get boolean value returned to us , which state that there is any apple with descendents of current node or not ,if yes we add '2' to our sum
 Time Complexity :- O(n+m)
 */
+class Solution {
+public:
+    int find(int node,vector<vector<int>>&tree,int n,vector<bool>&apple,vector<int>&vis,int& sum){
+        vis[node]++;
+        
+        int f = 0;
+        
+        for(auto&x:tree[node]){
+            if(vis[x])continue;
+            if(find(x,tree,n,apple,vis,sum)){
+                sum += 2;
+                f = 1;
+            }
+        }
+
+
+        if(f || apple[node])return true;
+        return false;
+    }
+
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        
+        vector<int>vis(n);
+        vector<vector<int>>tree(n);
+        for(auto&x:edges){
+            tree[x[0]].push_back(x[1]);
+            tree[x[1]].push_back(x[0]);
+        }
+        int sum = 0;
+        bool ans = find(0,tree,n,hasApple,vis,sum);
+        return sum;
+    }
+};
