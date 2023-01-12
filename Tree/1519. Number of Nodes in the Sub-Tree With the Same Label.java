f89682f28efbd4d4634085209f11076eb7a8d6f8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-12 22:19:10 +0530

    Update 1519. Number of Nodes in the Sub-Tree With the Same Label.java

--- Tree/1519. Number of Nodes in the Sub-Tree With the Same Label.java
+++ Tree/1519. Number of Nodes in the Sub-Tree With the Same Label.java
@@ -8,3 +8,35 @@ Approach :- We will do dfs and we want a frequency array which store the frequen
  Time Complexity :- O(n+e)
  Space Coplexity :- O(n+e+26)
 */
+class Solution {
+
+    public int[] find(int node,List<List<Integer>>tree,String labels,int ans[],int parent){
+        int fre[] = new int[26];
+        for(int x:tree.get(node)){
+            if(x == parent)continue;
+            int temp[] = find(x,tree,labels,ans,node);
+            for(int i=0;i<26;++i){
+                fre[i] += temp[i];
+            }
+        } 
+
+        fre[labels.charAt(node)-'a']++;
+        ans[node] = fre[labels.charAt(node)-'a'];
+        return fre;
+
+    }
+    public int[] countSubTrees(int n, int[][] edges, String labels) {
+        int ans[]= new int[n];
+        List<List<Integer>> tree = new ArrayList<>();
+        for(int i=0;i<n;++i)tree.add(new ArrayList<>());
+
+        for(int []x:edges){
+            tree.get(x[0]).add(x[1]);
+            tree.get(x[1]).add(x[0]);
+        }
+
+        int temp[] = find(0,tree,labels,ans,0);
+
+        return ans;
+    }
+}
