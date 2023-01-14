b48a2e5ced47c293f4aeea203fac7b2aba52f787
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-14 22:53:02 +0530

    Create 1061. Lexicographically Smallest Equivalent String.java

---
+++ Graph/DSU/1061. Lexicographically Smallest Equivalent String.java
@@ -0,0 +1,8 @@
+/*
+Platform :- Leetcode
+Approach :- If you will analyse the problem well you will find that there is connected graph is formed out of all equivalence relations and for each character we want to 
+            find the smallest character in Disjoint-set they form .
+            We will replace all the characters of Base String with their smallest possible character values in Disjoint set
+            We will create graph of connections and relations then we will find the smallest character for each value.
+Time Complexity :- O(n+e)
+*/
