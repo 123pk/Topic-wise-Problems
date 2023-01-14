e499cfc12611c96e4a432d7e854e30bf0ef01530
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-14 22:53:14 +0530

    Update 1061. Lexicographically Smallest Equivalent String.java

--- Graph/DSU/1061. Lexicographically Smallest Equivalent String.java
+++ Graph/DSU/1061. Lexicographically Smallest Equivalent String.java
@@ -6,3 +6,60 @@ Approach :- If you will analyse the problem well you will find that there is con
             We will create graph of connections and relations then we will find the smallest character for each value.
 Time Complexity :- O(n+e)
 */
+class Solution {
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        String ans="";
+        int vis[] = new int[26];
+        int score[] = new int[26];
+        List<List<Integer>> dsu = new ArrayList<>();
+        
+        for(int i=0;i<26;++i){
+            dsu.add(new ArrayList<>());
+        }
+         
+        for(int i=0;i<s1.length();++i){
+            char x = s1.charAt(i);
+            char y = s2.charAt(i);
+            dsu.get(x-'a').add(y-'a');
+            dsu.get(y-'a').add(x-'a');
+        }
+
+        for(int i=0;i<26;++i){
+            if(vis[i]>1)continue;
+
+            LinkedList<Integer>queue = new LinkedList<Integer>();
+            LinkedList<Integer>arr = new LinkedList<Integer>();;
+            queue.add(i);
+            vis[i]++;
+            int mn = 27;
+            while(queue.size()>0){
+                int node = queue.poll();
+                 
+                mn = Math.min(mn,node);
+                arr.add(node);
+
+                for(int x:dsu.get(node)){
+                    if(vis[x]>1)continue;
+                    queue.add(x);
+                    vis[x]++;
+                }
+            }
+
+            while(arr.size()>0){
+                int node = arr.poll();
+                 
+                score[node] = mn;
+            }
+        }
+         
+         for(int i=0;i<baseStr.length();++i){
+             char x = baseStr.charAt(i);
+             int sc = score[x-'a'];
+             char val = 'a';
+             val += sc;
+             ans += val;
+         }
+         return ans;
+
+    }
+}
