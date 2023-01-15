8a5740027a4b9992f78cbfb25c60d81301793b3d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-15 22:54:14 +0530

    Update 2537. Count the Number of Good Subarrays.cpp

--- Two pointer/2537. Count the Number of Good Subarrays.cpp
+++ Two pointer/2537. Count the Number of Good Subarrays.cpp
@@ -6,3 +6,36 @@ Approach :- We want to find no of subarrays satifying the following condition st
             moving a start pointer will cost us fre[nums[start]]-1 , where fre store the frequency of values of arrays till now.
 Time Compelxity :- O(n log n)
 */
+class Solution {
+public:
+    long long countGood(vector<int>& nums, int k) {
+        long long ans = 0;
+        
+        map<long long,long long>fre;
+        int start = 0; 
+        long long score = 0,sz = nums.size();
+        long long req = k;
+        
+        set<pair<long long,long long>>P;
+        for(long long i=0;i<nums.size();++i){
+            fre[nums[i]]++;
+            long long cur = fre[nums[i]];
+            score += (cur-1);
+            if(score>=k){
+            
+                ans += (sz-i);
+                //cout<<ans<<" ";
+                while(score>=k){
+                    score -= (fre[nums[start]]-1);
+                    fre[nums[start]]--;
+                    if(score>=k){
+                        ans += (sz-i);
+                    }
+                    start++;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
