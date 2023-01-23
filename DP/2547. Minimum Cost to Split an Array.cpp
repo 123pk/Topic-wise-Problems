5feb723f58d12a1588adc76656e0a40d63e3ee90
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-23 14:10:15 +0530

    Update 2547. Minimum Cost to Split an Array.cpp

--- DP/2547. Minimum Cost to Split an Array.cpp
+++ DP/2547. Minimum Cost to Split an Array.cpp
@@ -9,3 +9,34 @@ Approach :- We will use (Recursion + Memoisation) ,
             at the end the (score = importance_value + k + cost_from_(i-1) )
 Time Complexity :- O(n^2)
 */
+class Solution {
+public:
+    long long dp[1003];
+    long long find(int i,long long k,vector<int>&nums){
+        
+         if(dp[i]!=-1)return dp[i];
+        
+        int count[1003]={0};
+        long long c = 0,val=1e18;
+        for(int j=i;j>=0;--j){
+            count[nums[j]]++;
+            if(count[nums[j]] == 2){
+                c+=2;
+            }
+            else if(count[nums[j]]>2) c++;
+            long long cur = 0;
+            if((j-1)>=0)cur += find(j-1,k,nums);
+            val = min(val,cur+c+k);
+        }
+        //cout<<(i+1)<<" "<<val<<"\n";
+        return dp[i] = val;
+    }
+    
+    int minCost(vector<int>& nums, int k) {
+         memset(dp,-1,sizeof(dp));
+        int n = nums.size();
+        long long ex = k;
+       // cout<<"\n";
+        return find(n-1,ex,nums);
+    }
+};
