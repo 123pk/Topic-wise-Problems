2118b2a0f597c75fe4db94ee661954aede3e3927
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-24 14:06:10 +0530

    Create 2538. Difference Between Maximum and Minimum Price Sum.cpp

---
+++ Tree/2538. Difference Between Maximum and Minimum Price Sum.cpp
@@ -0,0 +1,11 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 328
+Approach :- We want two things from each child for each node , the ( not_removed_sum , removed_sum)
+            not_removed_sum - max sum of path of nodes ( we have not removed leaf_value)
+            removed_sum - max sum of path of nodes ( we have already removed leaf value )
+            
+            -- we want to find the max sum in both cases.
+               
+            
+*/
