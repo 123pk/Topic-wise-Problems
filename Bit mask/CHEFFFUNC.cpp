5139b662fdc9daca2f1430cc232f0595c5aed623
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-27 12:09:22 +0530

    Update CHEFFFUNC.cpp

--- Bit mask/CHEFFFUNC.cpp
+++ Bit mask/CHEFFFUNC.cpp
@@ -5,8 +5,8 @@ Approach :- You need to observe the values  of f(x) and g(x) for [1<= x <=32 ] ,
             After that we the value decreases .
             till it reach the next power_of_2.
             So approach is to find the biggest power of '2' in range 'a' to 'b' if there is no such value then we find the score for 
-            'a' as we have oberve the value will decrease from 'a'-->b as we move.
-            So the biggest will be at 'a' if no power of 2 in range.
+            first 100 values from 'a' to min(b,a+100) 
+            and the biggest will be our answr.
             Else the power of 2 will give use the max value.
 Time Complexity :- O(log(b))
 */
