cd0d0d85c7d8ef3be31056121733c68b634867a6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-27 22:26:21 +0530

    Update E - Vlad and a Pair of Numbers.cpp

--- Maths/E - Vlad and a Pair of Numbers.cpp
+++ Maths/E - Vlad and a Pair of Numbers.cpp
@@ -11,3 +11,85 @@ Approach :- If you will simplify the problem, 'x' = the xor of (a&b)
             now if frequency is '2' we add that value to both 'a' and b' else we can add to either.
 Time Complexity :- O(log^2 n)
 */
+#include<bits/stdc++.h>
+using namespace std;
+ 
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    
+    while(t--){
+      long long n;
+      cin>>n;
+      
+      string temp = bitset<32>(n).to_string();
+      string z = "";
+      int f = 0;
+      for(int i=0;i<32;++i){
+          if(f){
+              z+= temp[i];
+          }
+          else{
+              if(temp[i] == '1'){
+                  f=1;
+                  z += temp[i];
+              }
+          }
+      }
+      temp = z;
+      reverse(temp.begin(),temp.end());
+      
+      vector<long long>avail,req;
+      long long a = 0,b = 0;
+      for(long long i=0;i<temp.size();++i){
+          if(temp[i] == '0'){
+              avail.push_back(1ll<<i);
+              avail.push_back(1ll<<i);
+          }
+          else{
+              req.push_back(1ll<<i);
+              if(a == 0)a += 1ll<<i;
+              else{
+                  b += (1ll<<i);
+              }
+          }
+      }
+       
+      int len = avail.size();
+      sort(avail.begin(),avail.end());
+      sort(req.begin(),req.end());
+      map<long long,int>used;
+      
+      
+      int j = req.size()-1;
+      int i = avail.size()-1;
+      
+      while(j>=0 && i>=0){
+          if(avail[i]<=req[j]){
+              req[j] -= avail[i];
+              used[avail[i]]++;
+              if(req[j] == 0)j--;
+              i--;
+          }
+          else i--;
+      }
+       
+      
+      if(j == -1){
+          for(auto&x:used){
+              if(x.second == 2){
+                  a += x.first;
+                  b += x.first;
+              }
+              else a += x.first;
+          }
+          cout<<a<<" "<<b<<"\n";
+      }
+      else cout<<"-1\n";
+       
+      
+    }
+}
