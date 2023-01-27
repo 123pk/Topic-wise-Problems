c32bc06c076437b4c46a90406a943f612e0a548d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-01-27 12:14:00 +0530

    Update CHEFFFUNC.cpp

--- Bit mask/CHEFFFUNC.cpp
+++ Bit mask/CHEFFFUNC.cpp
@@ -10,3 +10,64 @@ Approach :- You need to observe the values  of f(x) and g(x) for [1<= x <=32 ] ,
             Else the power of 2 will give use the max value.
 Time Complexity :- O(log(b))
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+long long f1(long long x){
+    if(x == 1)return 1;
+    else{
+        if(x%2)return 2ll*f1(x/2);
+        else return 2ll*f1(x/2)+1;
+    }
+}
+
+long long f2(long long x){
+    if(x == 1)return 0;
+    else{
+        if(x%2)return f2(x/2);
+        else return f2(x/2)+1;
+    }
+}
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long a,b;
+        cin>>a>>b;
+        
+        //smallest value
+        long long ans = 0;
+        long long dif = (b-a);
+        for(long long i = 0;i<=min(100ll,dif);++i){
+            ans = max(ans,f1(a+i)+f2(a+i));
+        }
+        
+        //find the biggest multiple of f2
+        long long score = 1;
+        int f = 0;
+        while(score<b){
+            score*=2ll;
+        }
+        if(score>b)score/=2ll;
+        
+        if(a<=score && score<=b){
+            long long tot1 = 0,tot2 = 0;
+           // cout<<score<<"\n";
+            long long d = 0;
+            while(score >1){
+                score/=2;
+                d++;
+            }
+            
+            tot2 = (1ll<<d);
+            tot2*=2ll;
+            tot2--;
+          //  cout<<d<<" "<<tot2<<"\n";
+            tot1 = (d+tot2);
+            ans = max(ans,tot1);
+        }
+        cout<<ans<<"\n";
+    }
+}
