5079dea68a7cc734284be97b2b28b5c31daacf7c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-02-02 22:43:48 +0530

    Update and rename D.cpp to CONSTRRAY.cpp

---
+++ Constructive/CONSTRRAY.cpp
@@ -0,0 +1,8 @@
+
+/*
+Platform :- Codechef
+Contest :- Codechef Starters 76 B
+Approach :- we start from very big value and greedily add them so that they satisfy the condition,
+            for even and odd indexes as given,
+Time Complexity :- O(n)
+*/
