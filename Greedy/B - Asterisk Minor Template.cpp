c7968efffcf4e655b0bc592aaeffdf0efa0936a2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-02-28 23:10:48 +0530

    Create B - Asterisk Minor Template.cpp

---
+++ Greedy/B - Asterisk Minor Template.cpp
@@ -0,0 +1,9 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Educational ROund 144
+Approach :- We need to check for three things
+           1. If first element is equal or last element is equal
+           2. If there is any subarray of size 2 that is in both 
+           if any of this condition is satisfied then solution exist .
+ Time Complexity :- O(n)
+*/
